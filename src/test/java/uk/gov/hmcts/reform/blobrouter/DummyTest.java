640fa93023fa1dff186a8aa8581b2cee87f89098
Donatas Martinkus <doncem@users.noreply.github.com>
2019-10-23 15:15:47 +0100

    Add dummy unit test

---
+++ src/test/java/uk/gov/hmcts/reform/blobrouter/DummyTest.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.blobrouter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// remove once actual units arrive
+class DummyTest {
+
+    @Test
+    void should_verify_being_dummy_test() {
+        assertThat("I am dummy").isNotBlank();
+    }
+}
