d131e6bc2a9d3fd7ef7fec13aebe827ff2f68a9f
Donatas Martinkus <doncem@users.noreply.github.com>
2019-11-08 12:11:49 +0000

    Remove rest of setup

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,14 +1,3 @@
 provider "azurerm" {
   version = "=1.33.1"
 }
-
-locals {
-  tags = "${merge(var.common_tags, map("Team Contact", "#rbs"))}"
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.env}"
-  location = "${var.location}"
-
-  tags = "${local.tags}"
-}
