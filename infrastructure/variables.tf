1e2f8afe23bbae05aa535d7ce923eb51c4474a0e
Donatas Martinkus <doncem@users.noreply.github.com>
2019-11-08 12:08:26 +0000

    Remove key vault

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -30,7 +30,3 @@ variable "jenkins_AAD_objectId" {
   type        = "string"
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
-
-variable "managed_identity_object_id" {
-  default = ""
-}
