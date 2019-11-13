469935e3bd4b60b22f387dae7ec761d6fe747206
Aliveni Choppa <aliveni.ch@gmail.com>
2019-11-13 15:13:11 +0000

    remove unused import

--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/controllers/SasTokenControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/controllers/SasTokenControllerTest.java
@@ -5,7 +5,6 @@ import com.azure.storage.common.implementation.StorageImplUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
