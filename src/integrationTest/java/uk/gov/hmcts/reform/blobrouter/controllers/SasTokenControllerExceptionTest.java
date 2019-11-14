6b00fdabbf132c5ba188446e38ba78dcd442d3b7
aliveni <aliveni.choppa@hmcts.net>
2019-11-14 21:09:32 +0000

    Add controller advice to handle the exceptions (#18)

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/controllers/SasTokenControllerExceptionTest.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.reform.blobrouter.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.reform.blobrouter.exceptions.UnableToGenerateSasTokenException;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = SasTokenController.class)
+@ComponentScan(basePackages = "uk.gov.hmcts.reform.blobrouter")
+@TestPropertySource(properties = {
+    "storage.account-name = ",
+    "storage.account-key = "
+})
+public class SasTokenControllerExceptionTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_throw_exception_when_storage_is_not_configured() throws Exception {
+        mockMvc
+            .perform(get("/token/bulkscan"))
+            .andDo(print())
+            .andExpect(status().isInternalServerError())
+            .andExpect(jsonPath("$.message").value("Exception occurred while generating SAS Token"))
+            .andExpect(jsonPath("$.cause").value(UnableToGenerateSasTokenException.class.getName()));
+    }
+}
