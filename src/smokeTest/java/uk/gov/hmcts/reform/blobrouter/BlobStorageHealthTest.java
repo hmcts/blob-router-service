585718671421fdaed9d64fa5dc6a098d58c140d7
aliveni <aliveni.choppa@hmcts.net>
2019-11-16 11:43:21 +0000

    Add smoke test for the Storage sas token (#22)

---
+++ src/smokeTest/java/uk/gov/hmcts/reform/blobrouter/BlobStorageHealthTest.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.blobrouter;
+
+import com.typesafe.config.ConfigFactory;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.logging.appinsights.SyntheticHeaders;
+
+import static org.hamcrest.Matchers.containsString;
+
+public class BlobStorageHealthTest {
+
+    private static final String TEST_URL = ConfigFactory.load().getString("test-url");
+
+    @Test
+    public void should_get_the_sas_token_for_service() {
+        RestAssured
+            .given()
+            .relaxedHTTPSValidation()
+            .baseUri(TEST_URL)
+            .header(SyntheticHeaders.SYNTHETIC_TEST_SOURCE, "Blob router smoke test")
+            .get("/token/bulkscan")
+            .then()
+            .statusCode(200)
+            .body("sas_token", containsString("sig"));
+    }
+}
