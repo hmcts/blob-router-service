7267ba27fee5b30300fd0e0cba58ac37831a0d3f
donatas M <1420191+doncem@users.noreply.github.com>
2019-12-09 18:12:23 +0000

    Migrate Swagger v2 (SpringFox) to v3 (OpenApi) (#39)

--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/config/SwaggerPublisher.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/config/SwaggerPublisher.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.OutputStream;
@@ -19,7 +18,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Built-in feature which saves service's swagger specs in temporary directory.
  * Each travis run on master should automatically save and upload (if updated) documentation.
  */
-@SpringJUnitWebConfig
 @SpringBootTest
 @AutoConfigureMockMvc
 class SwaggerPublisher {
@@ -31,7 +29,7 @@ class SwaggerPublisher {
     @Test
     @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
     void generateDocs() throws Exception {
-        byte[] specs = mvc.perform(get("/v2/api-docs"))
+        byte[] specs = mvc.perform(get("/v3/api-docs"))
             .andExpect(status().isOk())
             .andReturn()
             .getResponse()
--- src/main/java/uk/gov/hmcts/reform/blobrouter/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/controllers/RootController.java
@@ -1,5 +1,9 @@
 package uk.gov.hmcts.reform.blobrouter.controllers;
 
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +13,14 @@ import static org.springframework.http.ResponseEntity.ok;
 /**
  * Default endpoints per application.
  */
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Blob Router",
+        version = "2",
+        description = "API serving SAS Tokens for uploads to Azure Blob Storage",
+        license = @License(name = "MIT", url = "https://opensource.org/licenses/MIT")
+    )
+)
 @RestController
 public class RootController {
 
@@ -22,6 +34,7 @@ public class RootController {
      * @return Welcome message from the service.
      */
     @GetMapping("/")
+    @Hidden
     public ResponseEntity<String> welcome() {
         return ok("Welcome to blob-router-service");
     }
