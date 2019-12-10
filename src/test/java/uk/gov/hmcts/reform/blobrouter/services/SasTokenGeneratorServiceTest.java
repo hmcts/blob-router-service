31d5a5900bf19cdde2deedacf6ae5980a476cf8b
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2019-12-10 12:02:29 +0000

    Set protocol for SAS token generation (#40)

--- src/test/java/uk/gov/hmcts/reform/blobrouter/services/SasTokenGeneratorServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/blobrouter/services/SasTokenGeneratorServiceTest.java
@@ -57,6 +57,7 @@ class SasTokenGeneratorServiceTest {
         OffsetDateTime expiresAt = OffsetDateTime.parse(queryParams.get("se")[0]); //expiry datetime for the signature
         assertThat(expiresAt).isBetween(now, now.plusSeconds(300));
         assertThat(queryParams.get("sp")).contains("wl");//access permissions(write-w,list-l)
+        assertThat(queryParams.get("spr")).containsExactlyInAnyOrder("https","http");
     }
 
     @Test
