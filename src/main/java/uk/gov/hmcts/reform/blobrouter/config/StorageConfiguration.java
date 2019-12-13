204441ce313189a26f5ba567757305fcdcbd4ac2
Nitin Prabhu <prabhunitin61@gmail.com>
2019-12-13 13:03:02 +0000

    Followed reference from data extractor

--- src/main/java/uk/gov/hmcts/reform/blobrouter/config/StorageConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/config/StorageConfiguration.java
@@ -10,8 +10,8 @@ public class StorageConfiguration {
 
     @Bean
     public StorageSharedKeyCredential getStorageSharedKeyCredential(
-        @Value("${storage.account_name}") String accountName,
-        @Value("${storage.account_key}") String accountKey
+        @Value("${storage.account-name}") String accountName,
+        @Value("${storage.account-key}") String accountKey
     ) {
         return new StorageSharedKeyCredential(accountName, accountKey);
     }
