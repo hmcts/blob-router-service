034ac59f0eb92b31bced3ab479b6839db8949c32
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2020-01-08 15:33:28 +0000

    db infra (#71)

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -10,9 +10,22 @@ variable "env" {
   type = "string"
 }
 
+variable "subscription" {
+  type = "string"
+}
+
+variable "common_tags" {
+  type = "map"
+}
+
 # list of SSL client certificate thumbprints that are accepted by the API (gateway)
 # (excludes certificates used by API tests)
 variable "allowed_client_certificate_thumbprints" {
   type    = "list"
   default = []
+}
+
+variable "location_db" {
+  type    = "string"
+  default = "UK South"
 }
