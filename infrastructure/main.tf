928b1388e6cadb4de8c21eed7f9cc7da082fc20f
Rafal Kondratowicz <rkondr@gmail.com>
2020-01-08 12:21:36 +0000

    Add db infra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,6 +7,20 @@ locals {
   bulk-scan-vault-name   = "bulk-scan-${var.env}"
 }
 
+module "blob-router-db" {
+  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+  product            = "${var.product}-${var.component}"
+  location           = "${var.location_db}"
+  env                = "${var.env}"
+  database_name      = "blob_router"
+  postgresql_user    = "blob_router"
+  postgresql_version = "10"
+  sku_name           = "GP_Gen5_2"
+  sku_tier           = "GeneralPurpose"
+  common_tags        = "${var.common_tags}"
+  subscription       = "${var.subscription}"
+}
+
 # region: key vault definitions
 
 data "azurerm_key_vault" "bulk_scan_key_vault" {
@@ -19,7 +33,7 @@ data "azurerm_key_vault" "reform_scan_key_vault" {
   resource_group_name = "reform-scan-${var.env}"
 }
 
-# end region
+# endregion
 
 # region: storage secrets from bulk scan
 
@@ -33,7 +47,7 @@ data "azurerm_key_vault_secret" "bulk_scan_storage_account_primary_key" {
   name         = "storage-account-primary-key"
 }
 
-# end region
+# endregion
 
 # region: copy CFT storage account secrets from bulk-scan key vault to reform-scan key vault
 
@@ -49,7 +63,7 @@ resource "azurerm_key_vault_secret" "bulkscan_storage_account_primary_key" {
   key_vault_id = "${data.azurerm_key_vault.reform_scan_key_vault.id}"
 }
 
-# end region
+# endregion
 
 # region: error notification secrets from bulk scan
 
@@ -68,7 +82,7 @@ data "azurerm_key_vault_secret" "bulk_scan_error_notifications_username" {
   name         = "error-notifications-username"
 }
 
-# end region
+# endregion
 
 # region: copy error notification secrets from bulk scan to reform scan
 
@@ -90,4 +104,11 @@ resource "azurerm_key_vault_secret" "error_notifications_username" {
   key_vault_id = "${data.azurerm_key_vault.reform_scan_key_vault.id}"
 }
 
-# end region
+# endregion
+
+# Copy postgres password for flyway migration
+resource "azurerm_key_vault_secret" "flyway_password" {
+  name         = "flyway-password"
+  value        = "${module.bulk-scan-db.postgresql_password}"
+  key_vault_id = "${data.azurerm_key_vault.reform_scan_key_vault.id}"
+}
