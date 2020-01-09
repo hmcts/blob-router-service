f6c2d93653111ca141dcdc53295b5a6c3be181a3
mdayican <mustafa.dayican@hmcts.net>
2020-01-09 13:09:55 +0000

    add value back

--- src/functionalTest/java/uk/gov/hmcts/reform/blobrouter/DummyTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/blobrouter/DummyTest.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.reform.blobrouter;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DummyTest {
 
+    @Value("${storage.crime.account-name}")
     private String crimeAccountName;
 
     @Test
@@ -15,7 +17,7 @@ class DummyTest {
 
     @Test
     void should_verify_config_value_being_dummy_test() {
-        System.out.println("value ------>"+crimeAccountName);
+        System.out.println("value ------>" + crimeAccountName);
         assertThat(crimeAccountName).isNotBlank();
         assertThat(crimeAccountName).isEqualTo("willfail");
     }
