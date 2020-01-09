bc2b05d49c8c53336a1d8381965401a6fac4b7f3
mdayican <mustafa.dayican@hmcts.net>
2020-01-09 14:58:57 +0000

    add log

--- src/main/java/uk/gov/hmcts/reform/blobrouter/config/StorageConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/config/StorageConfiguration.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.blobrouter.config;
 
 import com.azure.storage.common.StorageSharedKeyCredential;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +10,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StorageConfiguration {
 
+    private static final Logger log = LoggerFactory.getLogger(StorageConfiguration.class);
+
     @Bean
     public StorageSharedKeyCredential getStorageSharedKeyCredential(
         @Value("${storage.account-name}") String accountName,
         @Value("${storage.account-key}") String accountKey
     ) {
+
+        log.info("accountName: {}, accountKey: {}",accountName,accountKey);
         return new StorageSharedKeyCredential(accountName, accountKey);
     }
 }
