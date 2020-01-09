9cfb846e8c6eeec690e9fbbed6a4eced6cbc230f
Donatas Martinkus <doncem@users.noreply.github.com>
2020-01-09 19:02:41 +0000

    Add blob dispatcher component

--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/util/StorageStubHttpClient.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/util/StorageStubHttpClient.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.reform.blobrouter.util;
 
 import com.azure.core.http.HttpClient;
+import com.azure.core.http.HttpHeaders;
 import com.azure.core.http.HttpRequest;
 import com.azure.core.http.HttpResponse;
 import com.azure.core.test.http.MockHttpResponse;
 import org.junit.jupiter.api.Assertions;
 import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static uk.gov.hmcts.reform.blobrouter.util.ResourceFilesHelper.getFileContents;
 
@@ -14,6 +18,7 @@ class StorageStubHttpClient implements HttpClient {
     private static final String LIST_CONTAINERS = "?comp=list";
     private static final String LIST_ONE_BLOB = "/bulkscan?include=&restype=container&comp=list";
     private static final String LIST_ZERO_BLOBS = "/empty?include=&restype=container&comp=list";
+    private static final String UPLOAD_NEW_BLOB = "/bulkscan/new.blob?null";
 
     @Override
     public Mono<HttpResponse> send(HttpRequest request) {
@@ -32,6 +37,17 @@ class StorageStubHttpClient implements HttpClient {
             case LIST_ZERO_BLOBS:
                 return Mono.just(
                     new MockHttpResponse(request, 200, getFileContents("storage/list-empty-blobs.json"))
+                );
+            case UPLOAD_NEW_BLOB:
+                Map<String, String> headers = new HashMap<>();
+                headers.put("ETag", "0x8D761B5AF5CA061");
+                headers.put("Last-Modified", "Tue, 05 Nov 2019 06:02:05 GMT");
+                headers.put("x-ms-request-server-encrypted", "false");
+                headers.put("x-ms-encryption-key-sha256", "sha256");
+                request.getHeaders().put("x-ms-encryption-key-sha256", "sha256");
+
+                return Mono.just(
+                    new MockHttpResponse(request, 201, new HttpHeaders(headers))
                 );
             default:
                 Assertions.fail("Request '" + request.getUrl() + "' is not set up");
