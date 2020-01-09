69901b47d66d8497f4285a8eec5b7f899a265b43
mdayican <mustafa.dayican@hmcts.net>
2020-01-09 11:23:18 +0000

    add crime storage config

--- src/functionalTest/java/uk/gov/hmcts/reform/blobrouter/DummyTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/blobrouter/DummyTest.java
@@ -1,13 +1,28 @@
 package uk.gov.hmcts.reform.blobrouter;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DummyTest {
 
+    @Value("${storage.crime.account-name}")
+    private String crimeAccountName;
+
     @Test
     void should_verify_being_dummy_test() {
         assertThat("I am dummy").isNotBlank();
     }
+
+
+
+    @Test
+    void should_verify_config_value_being_dummy_test() {
+        System.out.println("value ------>"+crimeAccountName);
+        assertThat(crimeAccountName).isNotBlank();
+        assertThat(crimeAccountName).isEqualTo("willfail");
+    }
+
+
 }
