8ff58b05e235ed94df4e42df9271c69e88bfdddb
Donatas Martinkus <doncem@users.noreply.github.com>
2020-01-09 09:52:31 +0000

    Bump azure storage 12.0.1 -> 12.1.0 and test 1.0.0 -> 1.1.0

--- src/test/java/uk/gov/hmcts/reform/blobrouter/tasks/processors/ContainerProcessorTest.java
+++ src/test/java/uk/gov/hmcts/reform/blobrouter/tasks/processors/ContainerProcessorTest.java
@@ -1,31 +1,37 @@
 package uk.gov.hmcts.reform.blobrouter.tasks.processors;
 
+import com.azure.core.test.TestBase;
 import com.azure.storage.blob.BlobServiceAsyncClient;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import uk.gov.hmcts.reform.blobrouter.util.StorageClientsHelper;
-import uk.gov.hmcts.reform.blobrouter.util.StorageTestBase;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 @ExtendWith(OutputCaptureExtension.class)
 @SuppressWarnings("java:S2925") // ignore Thread.sleep() call in the tests
-class ContainerProcessorTest extends StorageTestBase {
+class ContainerProcessorTest extends TestBase {
 
     private static final String CONTAINER_WITH_BLOBS = "bulkscan";
     private static final String CONTAINER_WITHOUT_BLOBS = "empty";
 
     private ContainerProcessor containerProcessor;
 
-    private BlobServiceAsyncClient storageClient;
+    @BeforeAll
+    static void setUpTestMode() {
+        StorageClientsHelper.setAzureTestMode();
+
+        setupClass();
+    }
 
     @Override
     protected void beforeTest() {
-        storageClient = StorageClientsHelper.getStorageClient(interceptorManager);
+        BlobServiceAsyncClient storageClient = StorageClientsHelper.getStorageClient(interceptorManager);
         containerProcessor = new ContainerProcessor(storageClient);
     }
 
