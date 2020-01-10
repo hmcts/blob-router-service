58d72fc40491f1e86e727b1d19e0803fd1d8b227
Rafal Kondratowicz <rkondr@gmail.com>
2020-01-10 09:37:48 +0000

    Fixes

--- src/main/java/uk/gov/hmcts/reform/blobrouter/config/FlywayConfig.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/config/FlywayConfig.java
@@ -1,11 +1,23 @@
 package uk.gov.hmcts.reform.blobrouter.config;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.stream.Stream;
 
+@AutoConfigureAfter({DataSourceAutoConfiguration.class})
+@AutoConfigureBefore({FlywayAutoConfiguration.class})
+@Configuration
+@ConditionalOnClass(Flyway.class)
+@ConditionalOnProperty(prefix = "spring.flyway", name = "enabled", matchIfMissing = true)
 public class FlywayConfig {
 
     @Bean
