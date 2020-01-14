8566f99a7bd5a9eb94368f99f0a876ae9b76e990
Rafal Kondratowicz <rkondr@gmail.com>
2020-01-14 15:45:18 +0000

    Fix controller tests

--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/controllers/GetWelcomeTest.java
@@ -4,22 +4,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import uk.gov.hmcts.reform.blobrouter.data.EnvelopeRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = RootController.class)
-public class GetWelcomeTest {
+public class GetWelcomeTest extends ControllerTestBase {
 
     @Autowired
     private transient MockMvc mockMvc;
-
-    @MockBean private EnvelopeRepository envelopeRepo;
 
     @DisplayName("Should welcome upon root request with 200 response code")
     @Test
--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/controllers/SasTokenControllerExceptionTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/controllers/SasTokenControllerExceptionTest.java
@@ -3,11 +3,9 @@ package uk.gov.hmcts.reform.blobrouter.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import uk.gov.hmcts.reform.blobrouter.data.EnvelopeRepository;
 import uk.gov.hmcts.reform.blobrouter.exceptions.UnableToGenerateSasTokenException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -21,12 +19,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
     "storage.account-name = ",
     "storage.account-key = "
 })
-public class SasTokenControllerExceptionTest {
+public class SasTokenControllerExceptionTest extends ControllerTestBase {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean private EnvelopeRepository envelopeRepo;
 
     @Test
     public void should_throw_exception_when_storage_is_not_configured() throws Exception {
