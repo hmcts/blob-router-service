24430bd12bee453b8f1a3f7753742cd3a9001b72
donatas M <1420191+doncem@users.noreply.github.com>
2020-01-15 11:08:41 +0000

    Add blob processor (#89)

---
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/tasks/processors/BlobProcessor.java
@@ -0,0 +1,83 @@
+package uk.gov.hmcts.reform.blobrouter.tasks.processors;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.models.BlobStorageException;
+import com.azure.storage.blob.specialized.BlobLeaseClient;
+import com.azure.storage.blob.specialized.BlobLeaseClientBuilder;
+import org.slf4j.Logger;
+import uk.gov.hmcts.reform.blobrouter.services.storage.BlobDispatcher;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class BlobProcessor {
+
+    private static final Logger logger = getLogger(BlobProcessor.class);
+
+    private final BlobServiceClient storageClient;
+    private final BlobDispatcher dispatcher;
+
+    public BlobProcessor(
+        BlobServiceClient storageClient,
+        BlobDispatcher dispatcher
+    ) {
+        this.storageClient = storageClient;
+        this.dispatcher = dispatcher;
+    }
+
+    public void process(String blobName, String containerName) {
+        logger.info("Processing {} from {} container", blobName, containerName);
+
+        BlobLeaseClient leaseClient = null;
+
+        try {
+            BlobClient blobClient = storageClient
+                .getBlobContainerClient(containerName)
+                .getBlobClient(blobName);
+            leaseClient = new BlobLeaseClientBuilder()
+                .blobClient(blobClient)
+                .buildClient();
+
+            leaseClient.acquireLease(60);
+            byte[] rawBlob = tryToDownloadBlob(blobClient);
+
+            dispatcher.dispatch(blobName, rawBlob, containerName);
+
+            logger.info("Finished processing {} from {} container", blobName, containerName);
+        } catch (Exception exception) {
+            logger.error("Error occurred while processing {} from {}", blobName, containerName, exception);
+        } finally {
+            tryToReleaseLease(leaseClient, blobName, containerName);
+        }
+    }
+
+    public byte[] tryToDownloadBlob(BlobClient blobClient) throws IOException {
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            blobClient.download(outputStream);
+
+            return outputStream.toByteArray();
+        }
+    }
+
+    private void tryToReleaseLease(BlobLeaseClient leaseClient, String blobName, String containerName) {
+        try {
+            if (leaseClient != null) {
+                leaseClient.releaseLease();
+            }
+        } catch (BlobStorageException exception) {
+            // this will mean there was a problem acquiring lease in the first place
+            // or call to release the lease genuinely went wrong.
+            // warning as lease will expire anyway and normally should sort itself out
+            logger.warn(
+                "Could not release the lease with ID {}. Blob: {}, container: {}",
+                leaseClient.getLeaseId(),
+                blobName,
+                containerName,
+                exception
+            );
+        }
+    }
+}
