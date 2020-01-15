a9606dafb797144506aa50d385c318a1701fbaad
Rafal Kondratowicz <rkondr@gmail.com>
2020-01-15 16:57:37 +0000

    checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/blobrouter/config/StorageConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/config/StorageConfiguration.java
@@ -1,14 +1,14 @@
 package uk.gov.hmcts.reform.blobrouter.config;
 
-import com.azure.storage.blob.specialized.BlobLeaseClientBuilder;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.specialized.BlobLeaseClientBuilder;
 import com.azure.storage.common.StorageSharedKeyCredential;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import uk.gov.hmcts.reform.blobrouter.services.storage.LeaseClientProvider;
 import org.springframework.context.annotation.Profile;
+import uk.gov.hmcts.reform.blobrouter.services.storage.LeaseClientProvider;
 
 @Profile({"!integration-test"})
 @Configuration
