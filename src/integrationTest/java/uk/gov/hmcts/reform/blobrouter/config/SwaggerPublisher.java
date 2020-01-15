c55f4a1900b4f8538b0590c4b46c8790d540c928
Leszek Gonczar <gonczar@gmail.com>
2020-01-15 15:48:33 +0000

    Add storage account client beans for Bulk Scan and Crime (#91)

--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/config/SwaggerPublisher.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/config/SwaggerPublisher.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.OutputStream;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Built-in feature which saves service's swagger specs in temporary directory.
  * Each travis run on master should automatically save and upload (if updated) documentation.
  */
+@ActiveProfiles("integration-test")
 @SpringBootTest
 @AutoConfigureMockMvc
 class SwaggerPublisher {
--- src/main/java/uk/gov/hmcts/reform/blobrouter/config/StorageConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/config/StorageConfiguration.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.reform.blobrouter.config;
 
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.azure.storage.common.StorageSharedKeyCredential;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile({"!integration-test"})
 public class StorageConfiguration {
 
     @Bean
@@ -14,5 +18,19 @@ public class StorageConfiguration {
         @Value("${storage.account-key}") String accountKey
     ) {
         return new StorageSharedKeyCredential(accountName, accountKey);
+    }
+
+    @Bean("bulkscan-storage-client")
+    public static BlobServiceClient getBulkScanStorageClient(
+        @Value("${storage.bulkscan.connection-string}") String connectionString
+    ) {
+        return new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
+    }
+
+    @Bean("crime-storage-client")
+    public static BlobServiceClient getCrimeStorageClient(
+        @Value("${storage.crime.connection-string}") String connectionString
+    ) {
+        return new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
     }
 }
