c9ef89f784fb29903e638c952fdedc447b1d4bb2
mdayican <mustafa.dayican@hmcts.net>
2020-01-17 11:50:19 +0000

    add bulk-scan-processor client to get sas token (#119)
    * add bulk-scan-processor client to get sas token

---
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/clients/BulkScanProcessorClient.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.blobrouter.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@FeignClient(
+    name = "bulk-scan-processor-client",
+    url = "${bulk-scan-processor-url}"
+)
+public interface BulkScanProcessorClient {
+
+    @GetMapping(value = "/token/{service}", consumes = APPLICATION_JSON_VALUE)
+    SasTokenResponse getSasToken(@PathVariable String service);
+}
