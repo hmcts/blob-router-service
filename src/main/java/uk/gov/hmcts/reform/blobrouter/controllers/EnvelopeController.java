1f78a45bbcbfbab544cd978abb8f7a7fecb23a9b
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2020-01-21 15:22:22 +0000

    Add envelope controller (#151)

---
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/controllers/EnvelopeController.java
@@ -0,0 +1,46 @@
+package uk.gov.hmcts.reform.blobrouter.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.blobrouter.data.EnvelopeRepository;
+import uk.gov.hmcts.reform.blobrouter.data.model.Envelope;
+import uk.gov.hmcts.reform.blobrouter.exceptions.EnvelopeNotFoundException;
+import uk.gov.hmcts.reform.blobrouter.model.out.EnvelopeInfo;
+
+@RestController
+@RequestMapping(path = "/envelopes", produces = MediaType.APPLICATION_JSON_VALUE)
+public class EnvelopeController {
+
+    private final EnvelopeRepository envelopesRepo;
+
+    public EnvelopeController(EnvelopeRepository envelopesRepo) {
+        this.envelopesRepo = envelopesRepo;
+    }
+
+    @GetMapping()
+    public EnvelopeInfo findEnvelope(
+        @RequestParam("file_name") String fileName,
+        @RequestParam("container") String container
+    ) {
+        return envelopesRepo
+            .find(fileName, container)
+            .map(this::toResponse)
+            .orElseThrow(EnvelopeNotFoundException::new);
+    }
+
+    private EnvelopeInfo toResponse(Envelope dbEnvelope) {
+        return new EnvelopeInfo(
+            dbEnvelope.id,
+            dbEnvelope.container,
+            dbEnvelope.fileName,
+            dbEnvelope.createdAt,
+            dbEnvelope.fileCreatedAt,
+            dbEnvelope.dispatchedAt,
+            dbEnvelope.status,
+            dbEnvelope.isDeleted
+        );
+    }
+}
