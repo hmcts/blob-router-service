a6836b7f842114a7b7af98b3947107fb242be127
Rafal Kondratowicz <rkondr@gmail.com>
2020-01-23 15:42:58 +0000

    Clean up container cleaner test

--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/services/storage/ContainerCleanerIntTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/services/storage/ContainerCleanerIntTest.java
@@ -20,13 +20,11 @@ import uk.gov.hmcts.reform.blobrouter.data.EnvelopeRepository;
 import uk.gov.hmcts.reform.blobrouter.data.model.NewEnvelope;
 import uk.gov.hmcts.reform.blobrouter.data.model.Status;
 import uk.gov.hmcts.reform.blobrouter.tasks.processors.ContainerCleaner;
-import uk.gov.hmcts.reform.blobrouter.util.StorageClientsHelper;
 
 import java.io.File;
 import java.time.Instant;
 import java.util.List;
 
-import static com.azure.core.test.TestBase.setupClass;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.blobrouter.data.model.Status.DISPATCHED;
@@ -57,9 +55,7 @@ public class ContainerCleanerIntTest {
     private DbHelper dbHelper;
 
     @BeforeAll
-    static void setUpTestMode() {
-        StorageClientsHelper.setAzureTestMode();
-        setupClass();
+    static void beforeAll() {
 
         dockerComposeContainer =
             new DockerComposeContainer(new File("src/integrationTest/resources/docker-compose.yml"))
@@ -68,7 +64,7 @@ public class ContainerCleanerIntTest {
     }
 
     @AfterAll
-    static void tearDownTestMode() {
+    static void afterAll() {
         dockerComposeContainer.stop();
     }
 
