f96d4cb0bc7546fa893cfcddc59f722b50475ec9
donatas M <1420191+doncem@users.noreply.github.com>
2020-01-27 11:17:55 +0000

    Add bulkscan functional test (#161)

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/blobrouter/BulkScanBlobDispatchingTest.java
@@ -0,0 +1,68 @@
+package uk.gov.hmcts.reform.blobrouter;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.google.common.io.Resources.getResource;
+import static com.google.common.io.Resources.toByteArray;
+import static com.jayway.awaitility.Awaitility.await;
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.http.HttpStatus.OK;
+import static uk.gov.hmcts.reform.blobrouter.data.model.Status.DISPATCHED;
+import static uk.gov.hmcts.reform.blobrouter.envelope.ZipFileHelper.createZipArchive;
+import static uk.gov.hmcts.reform.blobrouter.storage.StorageHelper.blobExists;
+import static uk.gov.hmcts.reform.blobrouter.storage.StorageHelper.uploadFile;
+
+@Disabled("Functionality not available on bulk scan end")
+public class BulkScanBlobDispatchingTest extends FunctionalTestBase {
+
+    private static final String CONTAINER = "bulkscan";
+
+    @BeforeEach
+    @Override
+    protected void setUp() {
+        super.setUp();
+    }
+
+    @Test
+    void should_move_blob_to_bulkscan_storage() throws Exception {
+        // upload bulkscan file with unique name
+        String fileName = randomFileName();
+
+        byte[] internalZipContent = toByteArray(
+            getResource("test-data/envelope/envelope.zip")
+        );
+
+        byte[] wrappingZipContent = createZipArchive(
+            asList("test-data/envelope/envelope.zip", "test-data/envelope/signature")
+        );
+
+        // when
+        uploadFile(blobRouterStorageClient, CONTAINER, fileName, wrappingZipContent);
+
+        // then
+        await("Wait for the " + fileName + " to disappear from source container")
+            .atMost(2, TimeUnit.MINUTES)
+            .until(
+                () -> !blobExists(blobRouterStorageClient, CONTAINER, fileName)
+            );
+
+        // and
+        RestAssured
+            .given()
+            .baseUri(config.blobRouterUrl)
+            .relaxedHTTPSValidation()
+            .queryParam("file_name", fileName)
+            .queryParam("container", CONTAINER)
+            .get("/envelopes")
+            .then()
+            .statusCode(OK.value())
+            .body("$.status", equalTo(DISPATCHED))
+            .body("$.is_deleted", equalTo(true));
+    }
+}
--- src/functionalTest/java/uk/gov/hmcts/reform/blobrouter/config/TestConfiguration.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/blobrouter/config/TestConfiguration.java
@@ -5,11 +5,12 @@ import com.typesafe.config.ConfigFactory;
 
 public class TestConfiguration {
 
+    public final String blobRouterUrl;
+
     public final String sourceStorageAccountName;
     public final String sourceStorageAccountKey;
     public final String sourceStorageAccountUrl;
 
-    public final String bulkScanStorageUrl;
     public final String crimeStorageConnectionString;
 
     public final String crimeDestinationContainer;
@@ -18,11 +19,11 @@ public class TestConfiguration {
     public TestConfiguration() {
         Config config = ConfigFactory.load();
 
+        this.blobRouterUrl = config.getString("blob-router-url");
+
         this.sourceStorageAccountName = config.getString("source-storage-account-name");
         this.sourceStorageAccountKey = config.getString("source-storage-account-key");
         this.sourceStorageAccountUrl = config.getString("source-storage-account-url");
-
-        this.bulkScanStorageUrl = config.getString("bulkscan-storage-url");
 
         this.crimeStorageConnectionString = config.getString("crime-storage-connection-string");
         this.crimeDestinationContainer = config.getString("crime-source-container");
