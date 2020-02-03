d4aa84fc29b8f00bdd3a03bfa4e4d3aa840c2a1e
Rafal Kondratowicz <rkondr@gmail.com>
2020-02-03 12:13:39 +0100

    nicer usage

--- src/main/java/uk/gov/hmcts/reform/blobrouter/services/storage/RejectedFilesHandler.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/services/storage/RejectedFilesHandler.java
@@ -88,7 +88,7 @@ public class RejectedFilesHandler {
                 logger.info("Rejected file successfully handled. " + loggingContext);
             }
         } catch (Exception exc) {
-            logger.error("Error handling rejected file. " + loggingContext, exc);
+            logger.error("Error handling rejected file. {}", loggingContext, exc);
         }
     }
 
