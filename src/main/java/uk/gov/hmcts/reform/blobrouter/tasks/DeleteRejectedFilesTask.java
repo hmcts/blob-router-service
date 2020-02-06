ec645644ca75467025eeb2020aeee8f2e33c9ad7
Rafal Kondratowicz <rkondr@gmail.com>
2020-02-06 10:18:39 +0100

    Add scheduled job to delete rejected files

---
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/tasks/DeleteRejectedFilesTask.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.blobrouter.tasks;
+
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.slf4j.Logger;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.blobrouter.tasks.processors.RejectedContainerCleaner;
+
+import static org.slf4j.LoggerFactory.getLogger;
+import static uk.gov.hmcts.reform.blobrouter.util.TimeZones.EUROPE_LONDON;
+
+@Component
+@ConditionalOnProperty(value = "scheduling.task.delete-rejected-files.enabled")
+public class DeleteRejectedFilesTask {
+
+    private static final String TASK_NAME = "delete-rejected-files";
+    private static final Logger logger = getLogger(DeleteRejectedFilesTask.class);
+
+    private final RejectedContainerCleaner cleaner;
+
+    public DeleteRejectedFilesTask(RejectedContainerCleaner cleaner) {
+        this.cleaner = cleaner;
+    }
+
+    @Scheduled(cron = "${scheduling.task.delete-rejected-files.cron}", zone = EUROPE_LONDON)
+    @SchedulerLock(name = TASK_NAME)
+    public void run() {
+        logger.info("Started {} job", TASK_NAME);
+        cleaner.cleanUp();
+        logger.info("Finished {} job", TASK_NAME);
+    }
+}
