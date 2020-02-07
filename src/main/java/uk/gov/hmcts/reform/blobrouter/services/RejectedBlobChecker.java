97fdcf2320654b843bed336ffeb88d7f8ef80d1d
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2020-02-07 09:03:10 +0100

    Add scheduled job to delete rejected files (#222)

--- src/main/java/uk/gov/hmcts/reform/blobrouter/services/RejectedBlobChecker.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/services/RejectedBlobChecker.java
@@ -2,16 +2,21 @@ package uk.gov.hmcts.reform.blobrouter.services;
 
 import com.azure.storage.blob.models.BlobItem;
 import org.apache.commons.lang3.Validate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 
 import static java.time.OffsetDateTime.now;
 
+@Component
 public class RejectedBlobChecker {
 
     private final Duration ttl;
 
-    public RejectedBlobChecker(Duration ttl) {
+    public RejectedBlobChecker(
+        @Value("${scheduling.task.delete-rejected-files.ttl}") Duration ttl
+    ) {
         Validate.isTrue(ttl != null, "TTL is required");
         Validate.isTrue(!ttl.isNegative(), "TTL cannot be negative");
         this.ttl = ttl;
