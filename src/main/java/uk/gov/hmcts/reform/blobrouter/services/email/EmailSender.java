c6bf0a902d6879d872e5fa23051801bfdcbeb122
apereverzin <andrey.pereverzin@hotmail.com>
2020-02-07 16:39:55 +0000

    Added EmailSender

--- src/main/java/uk/gov/hmcts/reform/blobrouter/services/email/EmailSender.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/services/email/EmailSender.java
@@ -8,6 +8,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.util.Map;
 import javax.mail.internet.MimeMessage;
 
 @Component
@@ -24,13 +25,12 @@ public class EmailSender {
     }
     // endregion
 
-    public void sendMessageWithAttachment(
+    public void sendMessageWithAttachments(
         String subj,
         String body,
         String from,
         String[] recipients,
-        String fileName,
-        File file
+        Map<String, File> attachments
     ) throws SendEmailException {
         try {
             MimeMessage msg = mailSender.createMimeMessage();
@@ -40,7 +40,9 @@ public class EmailSender {
             helper.setTo(recipients);
             helper.setSubject(subj);
             helper.setText(body);
-            helper.addAttachment(fileName, file);
+            for (Map.Entry<String, File> attachment : attachments.entrySet()) {
+                helper.addAttachment(attachment.getKey(), attachment.getValue());
+            }
 
             mailSender.send(msg);
         } catch (Exception exc) {
--- src/test/java/uk/gov/hmcts/reform/blobrouter/services/email/EmailSenderTest.java
+++ src/test/java/uk/gov/hmcts/reform/blobrouter/services/email/EmailSenderTest.java
@@ -13,7 +13,9 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import uk.gov.hmcts.reform.blobrouter.jupiter.GreenMailExtension;
 
 import java.io.File;
+import java.util.Map;
 import java.util.Properties;
+import javax.activation.DataSource;
 import javax.mail.Address;
 import javax.mail.internet.MimeMessage;
 
@@ -32,7 +34,8 @@ public class EmailSenderTest {
     private static final String FROM_ADDRESS = "from@hmcts.net";
     private static final String RECIPIENT_1 = "Foo <foo@hmcts.net>";
     private static final String RECIPIENT_2 = "bar@hmcts.net";
-    public static final String FILE_NAME = "test.zip";
+    public static final String FILE_NAME_1 = "test1.zip";
+    public static final String FILE_NAME_2 = "test2.zip";
     public static final String SUBJ = "subj";
     public static final String BODY = "body";
 
@@ -46,21 +49,22 @@ public class EmailSenderTest {
         greenMail.setUser(TEST_LOGIN, TEST_PASSWORD);
         EmailSender emailSender = new EmailSender(getMailSender());
 
-        File file = new File(Resources.getResource(FILE_NAME).toURI());
+        File file1 = new File(Resources.getResource(FILE_NAME_1).toURI());
+        File file2 = new File(Resources.getResource(FILE_NAME_2).toURI());
 
         // when
-        emailSender.sendMessageWithAttachment(
+        emailSender.sendMessageWithAttachments(
             SUBJ,
             BODY,
             FROM_ADDRESS,
             new String[]{RECIPIENT_1, RECIPIENT_2},
-            FILE_NAME,
-            file
+            Map.of(FILE_NAME_1, file1, FILE_NAME_2, file2)
         );
 
         // then
         MimeMessageParser msg = new MimeMessageParser(greenMail.getReceivedMessages()[0]).parse();
 
+        assertThat(msg.getFrom()).isEqualTo(FROM_ADDRESS);
         assertThat(msg.getTo())
             .extracting(Address::toString)
             .hasSize(2)
@@ -70,8 +74,10 @@ public class EmailSenderTest {
             );
         assertThat(msg.getSubject()).isEqualTo(SUBJ);
         assertThat(msg.getPlainContent()).isEqualTo(BODY);
-        assertThat(msg.getAttachmentList()).hasSize(1);
-        assertThat(msg.getAttachmentList().get(0).getName()).isEqualTo(FILE_NAME);
+        assertThat(msg.getAttachmentList()).hasSize(2);
+        assertThat(msg.getAttachmentList().stream()
+                       .map(DataSource::getName))
+            .containsExactlyInAnyOrder(FILE_NAME_1, FILE_NAME_2);
     }
 
     @Test
@@ -88,16 +94,16 @@ public class EmailSenderTest {
 
         EmailSender emailSender = new EmailSender(mailSender);
 
-        File file = new File(Resources.getResource(FILE_NAME).toURI());
+        File file1 = new File(Resources.getResource(FILE_NAME_1).toURI());
+        File file2 = new File(Resources.getResource(FILE_NAME_2).toURI());
 
         // when
-        SendEmailException ex = catchThrowableOfType(() -> emailSender.sendMessageWithAttachment(
+        SendEmailException ex = catchThrowableOfType(() -> emailSender.sendMessageWithAttachments(
             SUBJ,
             BODY,
             FROM_ADDRESS,
             new String[]{RECIPIENT_1, RECIPIENT_2},
-            FILE_NAME,
-            file
+            Map.of(FILE_NAME_1, file1, FILE_NAME_2, file2)
         ), SendEmailException.class);
 
         // then
