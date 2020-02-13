89ec4cac13b2e4a4a940664e996db203ea3d2931
donatas M <1420191+doncem@users.noreply.github.com>
2020-02-13 13:48:22 +0000

    Add events table (#244)

---
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/data/EventRecordMapper.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.blobrouter.data;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.blobrouter.data.model.Event;
+import uk.gov.hmcts.reform.blobrouter.data.model.EventRecord;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class EventRecordMapper implements RowMapper<EventRecord> {
+
+    @Override
+    public EventRecord mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new EventRecord(
+            rs.getLong("id"),
+            rs.getString("container"),
+            rs.getString("file_name"),
+            rs.getTimestamp("created_at").toInstant(),
+            Event.valueOf(rs.getString("event")),
+            rs.getString("notes")
+        );
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/data/model/Event.java
@@ -0,0 +1,7 @@
+package uk.gov.hmcts.reform.blobrouter.data.model;
+
+public enum Event {
+    FILE_PROCESSING_STARTED,
+    DISPATCHED,
+    REJECTED,
+}
