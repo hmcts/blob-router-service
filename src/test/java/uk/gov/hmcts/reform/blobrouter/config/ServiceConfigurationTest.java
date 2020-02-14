cf9db8f6d85831d522c4cef08b11b467da31f584
Rafal Kondratowicz <rkondr@gmail.com>
2020-02-14 15:12:41 +0000

    Move method for finding enabled source containers

---
+++ src/test/java/uk/gov/hmcts/reform/blobrouter/config/ServiceConfigurationTest.java
@@ -0,0 +1,38 @@
+package uk.gov.hmcts.reform.blobrouter.config;
+
+import org.junit.jupiter.api.Test;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ServiceConfigurationTest {
+
+    @Test
+    void should_return_names_of_enabled_source_containers() {
+        // given
+        var conf = new ServiceConfiguration();
+        conf.setStorageConfig(
+            asList(
+                item("A", "A-rejected", true),
+                item("B", "B-rejected", true),
+                item("C", "C-rejected", true),
+                item("D", "D-rejected", false),
+                item("E", "E-rejected", false)
+            )
+        );
+
+        // when
+        var result = conf.getEnabledSourceContainers();
+
+        // then
+        assertThat(result).containsExactlyInAnyOrder("A", "B", "C");
+    }
+
+    private StorageConfigItem item(String sourceContainer, String targetContainer, boolean enabled) {
+        var item = new StorageConfigItem();
+        item.setSourceContainer(sourceContainer);
+        item.setTargetContainer(targetContainer);
+        item.setEnabled(enabled);
+        return item;
+    }
+}
