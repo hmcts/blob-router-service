7a9e841e42aebe9a817a0f7e3c2c2c9962ec7d5a
Rafal Kondratowicz <rkondr@gmail.com>
2020-02-24 11:15:49 +0100

    Cleanup

--- src/main/java/uk/gov/hmcts/reform/blobrouter/tasks/RejectDuplicatesTask.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/tasks/RejectDuplicatesTask.java
@@ -3,10 +3,8 @@ package uk.gov.hmcts.reform.blobrouter.tasks;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.blobrouter.config.ServiceConfiguration;
 import uk.gov.hmcts.reform.blobrouter.services.storage.DuplicateFileHandler;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -14,11 +12,10 @@ import static uk.gov.hmcts.reform.blobrouter.util.TimeZones.EUROPE_LONDON;
 
 @Component
 @ConditionalOnProperty(value = "scheduling.task.reject-duplicates.enabled")
-@EnableConfigurationProperties(ServiceConfiguration.class)
 public class RejectDuplicatesTask {
 
     private static final String TASK_NAME = "reject-duplicates";
-    private static final Logger logger = getLogger(HandleRejectedFilesTask.class);
+    private static final Logger logger = getLogger(RejectDuplicatesTask.class);
 
     private final DuplicateFileHandler duplicateFileHandler;
 
