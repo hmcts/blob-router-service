50b94aceb8ccf52d85d7959cf10aea39a49fa949
Rafal Kondratowicz <rkondr@gmail.com>
2020-02-24 16:52:31 +0100

    clean up imports

--- src/test/java/uk/gov/hmcts/reform/blobrouter/tasks/RejectDuplicatesTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/blobrouter/tasks/RejectDuplicatesTaskTest.java
@@ -1,9 +1,6 @@
 package uk.gov.hmcts.reform.blobrouter.tasks;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.blobrouter.services.storage.DuplicateFileHandler;
 
 import static org.mockito.Mockito.mock;
