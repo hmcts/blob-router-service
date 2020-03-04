69f998cf53a7929a6c8bcae5f2c4a17821aa4683
Rafal Kondratowicz <rkondr@gmail.com>
2020-03-04 18:07:02 +0000

    Fix

--- src/main/java/uk/gov/hmcts/reform/blobrouter/data/eventsv2/EnvelopeEventRepository.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/data/eventsv2/EnvelopeEventRepository.java
@@ -4,10 +4,12 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public class EnvelopeEventRepository {
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
