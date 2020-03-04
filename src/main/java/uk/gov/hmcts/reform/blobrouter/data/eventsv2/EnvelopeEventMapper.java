c59db5781ece69ba28e3353ed6c70aadfc945e34
Rafal Kondratowicz <rkondr@gmail.com>
2020-03-04 18:15:11 +0000

    fix

--- src/main/java/uk/gov/hmcts/reform/blobrouter/data/eventsv2/EnvelopeEventMapper.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/data/eventsv2/EnvelopeEventMapper.java
@@ -1,12 +1,14 @@
 package uk.gov.hmcts.reform.blobrouter.data.eventsv2;
 
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.blobrouter.data.events.EventType;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.UUID;
 
+@Component
 public class EnvelopeEventMapper implements RowMapper<EnvelopeEvent> {
     @Override
     public EnvelopeEvent mapRow(ResultSet rs, int rowNum) throws SQLException {
