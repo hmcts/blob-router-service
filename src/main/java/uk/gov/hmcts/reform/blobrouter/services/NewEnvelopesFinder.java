626432f4dcdfc33a0e98f07beef987ddb041f198
aliveni <aliveni.choppa@hmcts.net>
2020-03-06 14:52:36 +0000

    Add task for checking new envelopes in CFT and Crime containers (#341)

--- src/main/java/uk/gov/hmcts/reform/blobrouter/services/NewEnvelopesFinder.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/services/NewEnvelopesFinder.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.blobrouter.services;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.blobrouter.config.ServiceConfiguration;
 import uk.gov.hmcts.reform.blobrouter.data.envelopes.EnvelopeRepository;
 
@@ -13,6 +14,7 @@ import static java.time.temporal.ChronoUnit.HOURS;
 import static java.util.Collections.singleton;
 import static org.slf4j.LoggerFactory.getLogger;
 
+@Component
 public class NewEnvelopesFinder {
 
     private static final Logger logger = getLogger(NewEnvelopesFinder.class);
