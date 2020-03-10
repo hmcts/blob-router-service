dedfcd6292cc75de4beae02b0b556601f9778df7
Aliveni Choppa <aliveni.ch@gmail.com>
2020-03-10 23:13:45 +0000

    Update New Envelopes Finder service to check new envelopes in business hours only

--- src/main/java/uk/gov/hmcts/reform/blobrouter/services/NewEnvelopesFinder.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/services/NewEnvelopesFinder.java
@@ -8,10 +8,16 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.blobrouter.config.ServiceConfiguration;
 import uk.gov.hmcts.reform.blobrouter.data.envelopes.EnvelopeRepository;
 
+import java.time.Clock;
+import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import javax.validation.ClockProvider;
 
 import static java.util.Collections.singleton;
 import static org.slf4j.LoggerFactory.getLogger;
@@ -29,28 +35,40 @@ public class NewEnvelopesFinder {
 
     private static final String CRIME_CONTAINER = "crime";
 
+    private final Clock clock;
+
+    private static final int START_HOUR = 9;
+    private static final int END_HOUR = 18;
+    private static final List<DayOfWeek> WEEKEND = Arrays.asList(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
+
     public NewEnvelopesFinder(
         EnvelopeRepository envelopeRepository,
         ServiceConfiguration serviceConfig,
-        @Value("${scheduling.task.check-new-envelopes.time-interval}") Duration timeInterval
+        @Value("${scheduling.task.check-new-envelopes.time-interval}") Duration timeInterval,
+        ClockProvider clockProvider
     ) {
         Validate.isTrue(timeInterval != null, "Time interval is required");
         this.envelopeRepository = envelopeRepository;
         this.serviceConfig = serviceConfig;
         this.timeInterval = timeInterval;
+        this.clock = clockProvider.getClock();
     }
 
     public void checkNewCftEnvelopesCreated() {
-        checkNewEnvelopesInContainers(getCftContainers(), "CFT");
+        if (isCurrentTimeInBusinessHours(clock)) {
+            checkNewEnvelopesInContainers(getCftContainers(), "CFT");
+        }
     }
 
     public void checkNewCrimeEnvelopesCreated() {
-        if (serviceConfig.getEnabledSourceContainers().contains(CRIME_CONTAINER)) {
-            checkNewEnvelopesInContainers(singleton(CRIME_CONTAINER), "Crime");
-        } else {
-            logger.info(
-                "Not checking for new envelopes in {} container because container is disabled", CRIME_CONTAINER
-            );
+        if (isCurrentTimeInBusinessHours(clock)) {
+            if (serviceConfig.getEnabledSourceContainers().contains(CRIME_CONTAINER)) {
+                checkNewEnvelopesInContainers(singleton(CRIME_CONTAINER), "Crime");
+            } else {
+                logger.info(
+                    "Not checking for new envelopes in {} container because container is disabled", CRIME_CONTAINER
+                );
+            }
         }
     }
 
@@ -74,4 +92,9 @@ public class NewEnvelopesFinder {
             .collect(Collectors.toSet());
     }
 
+    private boolean isCurrentTimeInBusinessHours(Clock clock) {
+        LocalDateTime now = LocalDateTime.now(clock);
+        return !WEEKEND.contains(now.getDayOfWeek()) // not weekend
+            && now.getHour() > START_HOUR && now.getHour() < END_HOUR; // between 10am-6pm
+    }
 }
