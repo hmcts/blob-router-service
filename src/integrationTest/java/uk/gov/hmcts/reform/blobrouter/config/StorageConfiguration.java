30a6c6aedefbda89a0cf749a570a06792836d6f1
mdayican <mustafa.dayican@hmcts.net>
2020-03-11 15:21:51 +0000

    remove bean from integration config

--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/config/StorageConfiguration.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/config/StorageConfiguration.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.reform.blobrouter.config;
 
-import com.azure.core.http.HttpClient;
 import com.azure.storage.blob.BlobContainerClient;
-import com.azure.storage.blob.BlobContainerClientBuilder;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.azure.storage.blob.specialized.BlobLeaseClientBuilder;
@@ -10,11 +8,9 @@ import com.azure.storage.common.StorageSharedKeyCredential;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import uk.gov.hmcts.reform.blobrouter.services.storage.LeaseClientProvider;
 
 import static org.mockito.Mockito.mock;
-import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
 @Configuration
 public class StorageConfiguration {
@@ -36,14 +32,6 @@ public class StorageConfiguration {
     @Bean("crime-storage-client")
     public BlobContainerClient getCrimeContainerClient() {
         return mock(BlobContainerClient.class);
-    }
-
-    @Bean("bulk-scan-blob-client-builder")
-    @Scope(SCOPE_PROTOTYPE)
-    public BlobContainerClientBuilder getBulkScanBlobContainerClientBuilder() {
-        return new BlobContainerClientBuilder()
-            .httpClient(HttpClient.createDefault())
-            .endpoint("https://example.com");
     }
 
     @Bean
