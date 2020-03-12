57239cdc033b49dcd85c2ff6c32ed4a089b814f2
Rafal Kondratowicz <rkondr@gmail.com>
2020-03-12 13:26:45 +0100

    Create `BlobContentExtractor`

---
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/services/BlobContentExtractor.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.reform.blobrouter.services;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.blobrouter.config.TargetStorageAccount;
+import uk.gov.hmcts.reform.blobrouter.exceptions.InvalidZipArchiveException;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static com.google.common.io.ByteStreams.toByteArray;
+import static uk.gov.hmcts.reform.blobrouter.config.TargetStorageAccount.CRIME;
+import static uk.gov.hmcts.reform.blobrouter.util.zipverification.ZipVerifiers.ENVELOPE;
+
+@Component
+public class BlobContentExtractor {
+
+    public byte[] getContentToUpload(byte[] blobContent, TargetStorageAccount targetAccount) throws IOException {
+        if (targetAccount == CRIME) {
+            try (var zipStream = new ZipInputStream(new ByteArrayInputStream(blobContent))) {
+                ZipEntry entry;
+
+                while ((entry = zipStream.getNextEntry()) != null) {
+                    if (Objects.equals(entry.getName(), ENVELOPE)) {
+                        return toByteArray(zipStream);
+                    }
+                }
+
+                throw new InvalidZipArchiveException(
+                    String.format("ZIP file doesn't contain the required %s entry", ENVELOPE)
+                );
+            }
+        } else {
+           return blobContent;
+        }
+    }
+}
