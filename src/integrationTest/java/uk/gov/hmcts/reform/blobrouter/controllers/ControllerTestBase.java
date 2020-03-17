89bb66935d23f46b3a1c1b8b540c039836656684
Rafal Kondratowicz <rkondr@gmail.com>
2020-03-17 14:53:28 +0100

    Fix envelope controller test

--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/controllers/ControllerTestBase.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/controllers/ControllerTestBase.java
@@ -3,12 +3,10 @@ package uk.gov.hmcts.reform.blobrouter.controllers;
 import net.javacrumbs.shedlock.core.LockProvider;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.gov.hmcts.reform.blobrouter.config.ShedLockConfig;
-import uk.gov.hmcts.reform.blobrouter.data.envelopes.EnvelopeRepository;
-import uk.gov.hmcts.reform.blobrouter.data.events.EnvelopeEventRepository;
+import uk.gov.hmcts.reform.blobrouter.services.EnvelopeService;
 
 public class ControllerTestBase {
-    @MockBean protected EnvelopeRepository envelopeRepo;
-    @MockBean protected EnvelopeEventRepository eventRecordRepo;
+    @MockBean protected EnvelopeService envelopeService;
     @MockBean protected LockProvider lockProvider;
     @MockBean protected ShedLockConfig shedLockConfig;
 }
