780b7234c1caa361ad5e749625c264224a79cf39
Rafal Kondratowicz <rkondr@gmail.com>
2020-03-27 17:44:41 +0100

    Use the new leaser method

--- src/main/java/uk/gov/hmcts/reform/blobrouter/services/storage/LeaseAcquirer.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/services/storage/LeaseAcquirer.java
@@ -7,8 +7,6 @@ import com.azure.storage.blob.specialized.BlobLeaseClient;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Component
@@ -21,27 +19,6 @@ public class LeaseAcquirer {
 
     public LeaseAcquirer(LeaseClientProvider leaseClientProvider) {
         this.leaseClientProvider = leaseClientProvider;
-    }
-
-    @Deprecated
-    public Optional<BlobLeaseClient> acquireFor(BlobClient blobClient) {
-        try {
-            var leaseClient = leaseClientProvider.get(blobClient);
-            leaseClient.acquireLease(LEASE_DURATION_IN_SECONDS);
-            return Optional.of(leaseClient);
-
-        } catch (BlobStorageException exc) {
-            if (exc.getErrorCode() != BlobErrorCode.LEASE_ALREADY_PRESENT) {
-                logger.error(
-                    "Error acquiring lease for blob. File name: {}, Container: {}",
-                    blobClient.getBlobName(),
-                    blobClient.getContainerName(),
-                    exc
-                );
-            }
-
-            return Optional.empty();
-        }
     }
 
     public void ifAcquiredOrElse(BlobClient blobClient, Runnable onSuccess, Runnable onFailure) {
