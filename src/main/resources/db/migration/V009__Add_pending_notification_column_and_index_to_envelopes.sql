0c270231320ff413e96bccb838b9499d23eca43b
Aliveni Choppa <aliveni.ch@gmail.com>
2020-04-01 11:05:00 +0100

    Update index to have only pending_notification column

--- src/main/resources/db/migration/V009__Add_pending_notification_column_and_index_to_envelopes.sql
+++ src/main/resources/db/migration/V009__Add_pending_notification_column_and_index_to_envelopes.sql
@@ -1,4 +1,4 @@
 ALTER TABLE envelopes
 ADD COLUMN pending_notification BOOLEAN DEFAULT FALSE;
 
-CREATE INDEX envelopes_pending_notification_envelope_id_idx ON envelopes (id, pending_notification);
+CREATE INDEX envelopes_pending_notification_idx ON envelopes (pending_notification);
