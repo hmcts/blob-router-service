ce1e7a3089685e52c54314de291aceb507848e14
Nitin Prabhu <prabhunitin61@gmail.com>
2020-05-06 08:15:37 +0100

    Check secret bootstraps with same env var

--- src/main/java/uk/gov/hmcts/reform/blobrouter/Application.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/Application.java
@@ -1,15 +1,26 @@
 package uk.gov.hmcts.reform.blobrouter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import javax.sql.DataSource;
+
 @EnableFeignClients
 @SpringBootApplication
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired DataSource dataSource;
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("DATASOURCE = " + dataSource.toString());
     }
 }
