074bfba7e0435e4452997c6552170eb4d80b56c6
apereverzin <andrey.pereverzin@hotmail.com>
2020-05-16 13:20:08 +0100

    Added Sharable annotation to BulkScanProcessorClient

--- src/main/java/uk/gov/hmcts/reform/blobrouter/clients/bulkscanprocessor/BulkScanProcessorClient.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/clients/bulkscanprocessor/BulkScanProcessorClient.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.blobrouter.clients.bulkscanprocessor;
 
+import io.netty.channel.ChannelHandler;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
     name = "bulk-scan-processor-client",
     url = "${bulk-scan-processor-url}"
 )
+@ChannelHandler.Sharable
 public interface BulkScanProcessorClient {
 
     @GetMapping(value = "/token/{service}", consumes = APPLICATION_JSON_VALUE)
