7645b382314ab088be85bdbd807e843be1062a77
Donatas Martinkus <doncem@users.noreply.github.com>
2020-06-15 11:50:48 +0100

    Fix docblock main line. oops

--- src/main/java/uk/gov/hmcts/reform/blobrouter/services/storage/LeaseAcquirer.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/services/storage/LeaseAcquirer.java
@@ -26,7 +26,7 @@ public class LeaseAcquirer {
     }
 
     /**
-     * Main wrapper for blobs to be leased by {@link BlobLeaseClient} and perform deletion task.
+     * Main wrapper for blobs to be leased by {@link BlobLeaseClient} and perform non-delete task.
      * @param blobClient Represents blob
      * @param onSuccess Runnable task to perform when lease is acquired
      * @param onFailure Extra step to execute in case an error occurred
@@ -40,7 +40,7 @@ public class LeaseAcquirer {
     }
 
     /**
-     * Main wrapper for blobs to be leased by {@link BlobLeaseClient} and perform non-delete task.
+     * Main wrapper for blobs to be leased by {@link BlobLeaseClient} and perform deletion task.
      * @param blobClient Represents blob
      * @param onSuccess Consumer which takes in {@code leaseId} acquired with {@link BlobLeaseClient}
      * @param onFailure Extra step to execute in case an error occurred
