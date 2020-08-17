14724204cd5c675df343efcd7cb10c4e55038077
Aliveni Choppa <aliveni.ch@gmail.com>
2020-08-17 15:27:24 +0100

    Add @ApiModelProperty documentation to response

--- src/main/java/uk/gov/hmcts/reform/blobrouter/reconciliation/model/out/SuccessfulResponse.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/reconciliation/model/out/SuccessfulResponse.java
@@ -1,9 +1,14 @@
 package uk.gov.hmcts.reform.blobrouter.reconciliation.model.out;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 
 public class SuccessfulResponse {
 
+    @ApiModelProperty(
+        name = "ID",
+        notes = "ID under which Bulk Scanning has registered the report"
+    )
     @JsonProperty("id")
     public final String id;
 
