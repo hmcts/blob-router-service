9a4c0cee0f6032e81a6cfc3758c02984b30bf97d
Aliveni Choppa <aliveni.ch@gmail.com>
2020-08-20 08:14:04 +0100

    Remove Interceptor and add authorization header validation to Controller

--- src/main/java/uk/gov/hmcts/reform/blobrouter/reconciliation/controller/ReconciliationController.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/reconciliation/controller/ReconciliationController.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.reform.blobrouter.reconciliation.controller;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.blobrouter.exceptions.InvalidApiKeyException;
 import uk.gov.hmcts.reform.blobrouter.reconciliation.model.in.SupplierStatementReport;
 import uk.gov.hmcts.reform.blobrouter.reconciliation.model.out.SuccessfulResponse;
 import uk.gov.hmcts.reform.blobrouter.reconciliation.service.ReconciliationService;
@@ -27,8 +30,14 @@ public class ReconciliationController {
 
     private final ReconciliationService service;
 
-    public ReconciliationController(ReconciliationService service) {
+    private final String apiKey;
+
+    public ReconciliationController(
+        ReconciliationService service,
+        @Value("${reconciliation.api-key}") String apiKey
+    ) {
         this.service = service;
+        this.apiKey = apiKey;
     }
 
     @PostMapping(
@@ -45,11 +54,25 @@ public class ReconciliationController {
         @ApiResponse(code = 401, message = "Invalid API Key")
     })
     public SuccessfulResponse uploadSupplierReport(
-        @RequestHeader(AUTHORIZATION) final String authHeader,
+        @RequestHeader(value = AUTHORIZATION, required = false) String authHeader,
         @PathVariable("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
         @Valid @RequestBody SupplierStatementReport report
     ) {
+        validateAuthorization(authHeader);
         UUID uuid = service.saveSupplierStatement(date, report.supplierStatement);
         return new SuccessfulResponse(uuid.toString());
+    }
+
+    private void validateAuthorization(String authorizationKey) {
+
+        if (StringUtils.isEmpty(authorizationKey)) {
+            throw new InvalidApiKeyException("API Key is missing");
+        } else if (
+            !authorizationKey.startsWith("Bearer ")
+                || !StringUtils.substringAfter(authorizationKey, "Bearer ").equals(apiKey)
+        ) {
+            throw new InvalidApiKeyException("Invalid API Key");
+        }
+
     }
 }
