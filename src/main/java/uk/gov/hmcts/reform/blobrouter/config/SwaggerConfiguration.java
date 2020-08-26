0cb33b8071987f4fd1fb8707920b5b836fca2474
Aliveni Choppa <aliveni.ch@gmail.com>
2020-08-26 13:37:55 +0100

    Fix swagger configuration

--- src/main/java/uk/gov/hmcts/reform/blobrouter/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/config/SwaggerConfiguration.java
@@ -3,10 +3,12 @@ package uk.gov.hmcts.reform.blobrouter.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import uk.gov.hmcts.reform.blobrouter.Application;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 
 @Configuration
 @EnableSwagger2
@@ -17,7 +19,8 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
             .useDefaultResponseMessages(false)
             .select()
-            .apis(RequestHandlerSelectors.any())
+            .apis(basePackage(Application.class.getPackage().getName() + ".controllers")
+                      .or(basePackage(Application.class.getPackage().getName() + ".reconciliation.controller")))
             .paths(PathSelectors.any())
             .build();
     }
