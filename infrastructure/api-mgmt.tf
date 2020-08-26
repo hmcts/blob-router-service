be867ab76651f78ee7b4b7e15fbff7981265506a
Aliveni Choppa <aliveni.ch@gmail.com>
2020-08-26 12:34:51 +0100

    Add reconciliation endpoint to API management

--- infrastructure/api-mgmt.tf
+++ infrastructure/api-mgmt.tf
@@ -46,3 +46,27 @@ module "api_mgmt_policy" {
   api_name               = "${module.api_mgmt.name}"
   api_policy_xml_content = "${replace(file("api-policy.xml"), "ALLOWED_CERTIFICATE_THUMBPRINTS", local.quoted_thumbprints)}"
 }
+
+module "api_mgmt_reconciliation" {
+  source        = "git@github.com:hmcts/cnp-module-api-mgmt-api?ref=master"
+  name          = "${var.component}-reconciliation-api"
+  api_mgmt_name = "core-api-mgmt-${var.env}"
+  api_mgmt_rg   = "core-infra-${var.env}"
+  revision      = "1"
+  product_id    = "${module.api_mgmt_product.product_id}"
+  display_name  = "Blob Router Reconciliation API"
+  path          = "reform-scan/reconciliation-report"
+  protocols = [
+    "https"
+  ]
+  service_url = "http://${var.product}-${var.component}-${var.env}.service.core-compute-${var.env}.internal"
+  swagger_url = "https://hmcts.github.io/reform-api-docs/specs/blob-router-service.json"
+}
+
+module "api_mgmt_reconciliation_policy" {
+  source                 = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
+  api_mgmt_name          = "core-api-mgmt-${var.env}"
+  api_mgmt_rg            = "core-infra-${var.env}"
+  api_name               = "${module.api_mgmt_reconciliation.name}"
+  api_policy_xml_content = "${replace(file("reconciliation-api-policy.xml"), "ENV_NAME", var.env)}"
+}
