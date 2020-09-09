361ea05b0e3c7578787c646f3f8abdfc9e464583
Aliveni Choppa <aliveni.ch@gmail.com>
2020-09-09 09:40:09 +0100

    Remove `consumes` from PCqClient

--- src/main/java/uk/gov/hmcts/reform/blobrouter/clients/pcq/PcqClient.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/clients/pcq/PcqClient.java
@@ -5,14 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import uk.gov.hmcts.reform.blobrouter.clients.response.SasTokenResponse;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-
 @FeignClient(
     name = "pcq-backend-client",
     url = "${pcq-backend-api-url}"
 )
 public interface PcqClient {
 
-    @GetMapping(value = "/pcq/backend/token/bulkscan", consumes = APPLICATION_JSON_VALUE)
+    @GetMapping("/pcq/backend/token/bulkscan")
     SasTokenResponse getSasToken(@RequestHeader("ServiceAuthorization") String serviceAuthorisation);
 }
