3fcdab5785dab863f3616907d0ad95bb0bf6e02f
Aliveni Choppa <aliveni.ch@gmail.com>
2020-09-10 14:56:12 +0100

    Merge api-policies and rename api-policy file name to apply the policy changes

--- infrastructure/api-mgmt.tf
+++ infrastructure/api-mgmt.tf
@@ -36,7 +36,7 @@ module "api_mgmt" {
     "https"
   ]
   service_url = "http://${var.product}-${var.component}-${var.env}.service.core-compute-${var.env}.internal"
-  swagger_url = "https://hmcts.github.io/reform-api-docs/specs/blob-router-service.json"
+  swagger_url = "https://hmcts.github.io/reform-api-docs/specs/blob-router-service.json?v2"
 }
 
 module "api_mgmt_policy" {
@@ -44,29 +44,5 @@ module "api_mgmt_policy" {
   api_mgmt_name          = "core-api-mgmt-${var.env}"
   api_mgmt_rg            = "core-infra-${var.env}"
   api_name               = "${module.api_mgmt.name}"
-  api_policy_xml_content = "${replace(file("api-policy.xml"), "ALLOWED_CERTIFICATE_THUMBPRINTS", local.quoted_thumbprints)}"
-}
-
-module "api_mgmt_reconciliation" {
-  source        = "git@github.com:hmcts/cnp-module-api-mgmt-api?ref=master"
-  name          = "${var.component}-reconciliation-api"
-  api_mgmt_name = "core-api-mgmt-${var.env}"
-  api_mgmt_rg   = "core-infra-${var.env}"
-  revision      = "1"
-  product_id    = "${module.api_mgmt_product.product_id}"
-  display_name  = "Blob Router Reconciliation API"
-  path          = "reconciliation"
-  protocols = [
-    "https"
-  ]
-  service_url = "http://${var.product}-${var.component}-${var.env}.service.core-compute-${var.env}.internal"
-  swagger_url = "https://hmcts.github.io/reform-api-docs/specs/blob-router-service.json"
-}
-
-module "api_mgmt_reconciliation_policy" {
-  source                 = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
-  api_mgmt_name          = "core-api-mgmt-${var.env}"
-  api_mgmt_rg            = "core-infra-${var.env}"
-  api_name               = "${module.api_mgmt_reconciliation.name}"
-  api_policy_xml_content = "${replace(file("reconciliation-api-policy.xml"), "ENV_NAME", var.env)}"
+  api_policy_xml_content = "${replace(file("api-policy-v2.xml"), "ALLOWED_CERTIFICATE_THUMBPRINTS", local.quoted_thumbprints)}"
 }
