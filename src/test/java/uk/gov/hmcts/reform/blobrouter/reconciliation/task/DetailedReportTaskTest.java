ee4e9490eb165e0b0670657341608ce16a218147
MUSTAFA <mustafa.dayican@hmcts.net>
2020-09-11 11:58:53 +0100

    add DetailedReportTask (#669)

---
+++ src/test/java/uk/gov/hmcts/reform/blobrouter/reconciliation/task/DetailedReportTaskTest.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.blobrouter.reconciliation.task;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.blobrouter.reconciliation.service.DetailedReportService;
+
+import java.time.LocalDate;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.blobrouter.config.TargetStorageAccount.BULKSCAN;
+
+class DetailedReportTaskTest {
+
+    @Test
+    void should_call_summary_report_service() {
+        // given
+        var detailedReportService = mock(DetailedReportService.class);
+
+        var task = new DetailedReportTask(detailedReportService);
+
+        // when
+        task.run();
+
+        // then
+        verify(detailedReportService, times(1)).process(LocalDate.now(), BULKSCAN);
+    }
+
+}
