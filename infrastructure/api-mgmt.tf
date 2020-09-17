18b4ffe3b7d9b7e7b8d402adb8f81a0a86f6e02a
Donatas Martinkus <doncem@users.noreply.github.com>
2020-09-17 11:27:50 +0300

    Convert to HCL2 expressions

--- infrastructure/api-mgmt.tf
+++ infrastructure/api-mgmt.tf
@@ -1,26 +1,26 @@
 locals {
   # certificate thumbprints used by API tests
   allowed_test_certificate_thumbprints = [
-    "${var.api_test_valid_certificate_thumbprint}",
-    "${var.api_test_expired_certificate_thumbprint}",
-    "${var.api_test_not_yet_valid_certificate_thumbprint}",
+    var.api_test_valid_certificate_thumbprint,
+    var.api_test_expired_certificate_thumbprint,
+    var.api_test_not_yet_valid_certificate_thumbprint,
   ]
 
   # list of the thumbprints of the SSL certificates that should be accepted by the API (gateway)
-  allowed_certificate_thumbprints = "${concat(
+  allowed_certificate_thumbprints = concat(
     local.allowed_test_certificate_thumbprints,
     var.allowed_client_certificate_thumbprints
-  )}"
+  )
 
-  formatted_thumbprints = "${formatlist("&quot;%s&quot;", local.allowed_certificate_thumbprints)}"
-  quoted_thumbprints    = "${join(",", local.formatted_thumbprints)}"
+  formatted_thumbprints = formatlist("&quot;%s&quot;", local.allowed_certificate_thumbprints)
+  quoted_thumbprints    = join(",", local.formatted_thumbprints)
 }
 
 module "api_mgmt_product" {
   source        = "git@github.com:hmcts/cnp-module-api-mgmt-product?ref=master"
   api_mgmt_name = "core-api-mgmt-${var.env}"
   api_mgmt_rg   = "core-infra-${var.env}"
-  name          = "${var.component}"
+  name          = var.component
 }
 
 module "api_mgmt" {
@@ -29,7 +29,7 @@ module "api_mgmt" {
   api_mgmt_name = "core-api-mgmt-${var.env}"
   api_mgmt_rg   = "core-infra-${var.env}"
   revision      = "1"
-  product_id    = "${module.api_mgmt_product.product_id}"
+  product_id    = module.api_mgmt_product.product_id
   display_name  = "Blob Router API"
   path          = "reform-scan"
   protocols = [
@@ -43,6 +43,6 @@ module "api_mgmt_policy" {
   source                 = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
   api_mgmt_name          = "core-api-mgmt-${var.env}"
   api_mgmt_rg            = "core-infra-${var.env}"
-  api_name               = "${module.api_mgmt.name}"
-  api_policy_xml_content = "${replace(file("api-policy-v2.xml"), "ALLOWED_CERTIFICATE_THUMBPRINTS", local.quoted_thumbprints)}"
+  api_name               = module.api_mgmt.name
+  api_policy_xml_content = replace(file("api-policy-v2.xml"), "ALLOWED_CERTIFICATE_THUMBPRINTS", local.quoted_thumbprints)
 }
