5550cc3c7ddbe4a4896032bf7c43dac94596d8d1
mdayican <mustafa.dayican@hmcts.net>
2020-10-01 14:54:22 +0100

    add container validation to ReconciliationService

--- src/main/java/uk/gov/hmcts/reform/blobrouter/exceptions/InvalidSupplierStatementException.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/exceptions/InvalidSupplierStatementException.java
@@ -4,4 +4,8 @@ public class InvalidSupplierStatementException extends RuntimeException {
     public InvalidSupplierStatementException(String message, Exception cause) {
         super(message, cause);
     }
+
+    public InvalidSupplierStatementException(String message) {
+        super(message);
+    }
 }
--- src/main/java/uk/gov/hmcts/reform/blobrouter/reconciliation/service/ReconciliationService.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/reconciliation/service/ReconciliationService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import uk.gov.hmcts.reform.blobrouter.config.ServiceConfiguration;
 import uk.gov.hmcts.reform.blobrouter.data.reconciliation.reports.ReconciliationReportRepository;
 import uk.gov.hmcts.reform.blobrouter.data.reconciliation.reports.model.ReconciliationReport;
 import uk.gov.hmcts.reform.blobrouter.data.reconciliation.statements.SupplierStatementRepository;
@@ -14,6 +15,7 @@ import uk.gov.hmcts.reform.blobrouter.reconciliation.model.in.SupplierStatement;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -24,20 +26,24 @@ public class ReconciliationService {
     private final SupplierStatementRepository statementRepo;
     private final ReconciliationReportRepository reportRepo;
     private final ObjectMapper objectMapper;
+    private final ServiceConfiguration serviceConfig;
 
     public ReconciliationService(
         SupplierStatementRepository statementRepo,
         ReconciliationReportRepository reportRepo,
-        ObjectMapper objectMapper
+        ObjectMapper objectMapper,
+        ServiceConfiguration serviceConfig
     ) {
         this.statementRepo = statementRepo;
         this.reportRepo = reportRepo;
         this.objectMapper = objectMapper;
+        this.serviceConfig = serviceConfig;
     }
 
     @Transactional
     public UUID saveSupplierStatement(LocalDate date, SupplierStatement inputSupplierStatement) {
         try {
+            validateContainers(inputSupplierStatement);
             String supplierStatement = objectMapper.writeValueAsString(inputSupplierStatement);
             var statement = new NewEnvelopeSupplierStatement(
                 date,
@@ -47,6 +53,28 @@ public class ReconciliationService {
             return statementRepo.save(statement);
         } catch (JsonProcessingException | SQLException e) {
             throw new InvalidSupplierStatementException("Failed to process Supplier statement", e);
+        }
+    }
+
+    private void validateContainers(SupplierStatement inputSupplierStatement) {
+        if (inputSupplierStatement.envelopes != null) {
+            List<String> unrecognizedContainers = new ArrayList<String>();
+            inputSupplierStatement
+                .envelopes
+                .stream()
+                .map(e -> e.container)
+                .distinct()
+                .forEach(c -> {
+                    if (!serviceConfig.getSourceContainers().contains(c.toLowerCase())) {
+                        unrecognizedContainers.add(c);
+                    }
+                });
+
+            if (!unrecognizedContainers.isEmpty()) {
+                throw new InvalidSupplierStatementException(
+                    "Invalid statement. Unrecognized Containers :"
+                        + unrecognizedContainers.toString());
+            }
         }
     }
 
