17a7e1572dffedc138a24ca9658c3f5514bf2600
MUSTAFA <mustafa.dayican@hmcts.net>
2020-12-11 15:02:19 +0000

    remove proxy (#832)

--- src/main/java/uk/gov/hmcts/reform/blobrouter/config/HttpConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/config/HttpConfiguration.java
@@ -1,21 +1,16 @@
 package uk.gov.hmcts.reform.blobrouter.config;
 
 import com.azure.core.http.HttpClient;
-import com.azure.core.http.ProxyOptions;
 import com.azure.core.http.netty.NettyAsyncHttpClientBuilder;
 import feign.Client;
 import feign.httpclient.ApacheHttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import java.net.InetSocketAddress;
 
 @Configuration
 public class HttpConfiguration {
@@ -36,27 +31,7 @@ public class HttpConfiguration {
     }
 
     @Bean
-    @ConditionalOnProperty(prefix = "proxy", name = "enabled", havingValue = "true")
-    public HttpClient azureHttpClientWithProxy(
-        @Value("${proxy.host-name}") String proxyHostName,
-        @Value("${proxy.port}") int proxyPort
-    ) {
-        return new NettyAsyncHttpClientBuilder()
-            .proxy(
-                new ProxyOptions(
-                    ProxyOptions.Type.HTTP,
-                    new InetSocketAddress(
-                        proxyHostName,
-                        proxyPort
-                    )
-                )
-            )
-            .build();
-    }
-
-    @Bean
-    @ConditionalOnProperty(prefix = "proxy", name = "enabled", havingValue = "false")
-    public HttpClient azureHttpClientWithoutProxy() {
+    public HttpClient azureHttpClient() {
         return new NettyAsyncHttpClientBuilder().build();
     }
 
