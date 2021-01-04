4cee1f0f6d24e7538b8e7caf0190cce24379ae5c
Jason Paige <jasonpaige@users.noreply.github.com>
2021-01-04 14:39:18 +0000

    Out of memory fix (#813)

--- src/main/java/uk/gov/hmcts/reform/blobrouter/util/zipverification/ZipVerifiers.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/util/zipverification/ZipVerifiers.java
@@ -4,73 +4,59 @@ import uk.gov.hmcts.reform.blobrouter.exceptions.DocSignatureFailureException;
 import uk.gov.hmcts.reform.blobrouter.exceptions.InvalidZipArchiveException;
 import uk.gov.hmcts.reform.blobrouter.exceptions.SignatureValidationException;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.Signature;
 import java.security.SignatureException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-
-import static com.google.common.io.ByteStreams.toByteArray;
-import static java.util.Arrays.asList;
 
 public class ZipVerifiers {
 
     public static final String ENVELOPE = "envelope.zip";
     public static final String SIGNATURE = "signature";
+    public static final int BUFFER_SIZE = 1024;
 
     public static final String INVALID_SIGNATURE_MESSAGE = "Zip signature failed verification";
 
     private ZipVerifiers() {
     }
 
-    public static ZipInputStream verifyZip(ZipInputStream zipInputStream, PublicKey publicKey) {
-        Map<String, byte[]> zipEntries = extractZipEntries(zipInputStream);
-
-        verifyFileNames(zipEntries.keySet());
-        verifySignature(
-            publicKey,
-            zipEntries.get(ENVELOPE),
-            zipEntries.get(SIGNATURE)
-        );
-
-        return new ZipInputStream(new ByteArrayInputStream(zipEntries.get(ENVELOPE)));
-    }
-
-    private static Map<String, byte[]> extractZipEntries(ZipInputStream zis) {
+    public static void verifyZip(ZipInputStream zis, PublicKey publicKey) throws IOException {
         try {
-            Map<String, byte[]> zipEntries = new HashMap<>();
-            ZipEntry zipEntry;
-            while ((zipEntry = zis.getNextEntry()) != null) {
-                zipEntries.put(zipEntry.getName(), toByteArray(zis));
-            }
 
-            return zipEntries;
-        } catch (IOException ioe) {
-            throw new InvalidZipArchiveException("Error extracting zip entries", ioe);
-        }
-    }
-
-    public static void verifyFileNames(Set<String> fileNames) {
-        if (!(fileNames.size() == 2 && fileNames.containsAll(asList(ENVELOPE, SIGNATURE)))) {
-            throw new InvalidZipArchiveException(
-                "Zip entries do not match expected file names. Actual names = " + fileNames
-            );
-        }
-    }
-
-    public static void verifySignature(PublicKey publicKey, byte[] data, byte[] signed) {
-        try {
             Signature signature = Signature.getInstance("SHA256withRSA");
             signature.initVerify(publicKey);
-            signature.update(data);
-            if (!signature.verify(signed)) {
+            byte[] signatureByteArray = null;
+
+            ZipEntry zipEntry;
+            while ((zipEntry = zis.getNextEntry()) != null) {
+
+                if (zipEntry.getName().equalsIgnoreCase(ENVELOPE)) {
+
+                    byte[] envelopeData = new byte[BUFFER_SIZE];
+                    while (zis.available() != 0) {                      
+                        int numBytesRead = zis.readNBytes(envelopeData, 0, BUFFER_SIZE);
+                        signature.update(envelopeData, 0, numBytesRead);
+                    }
+                } else if (zipEntry.getName().equalsIgnoreCase(SIGNATURE)) {
+                    signatureByteArray = zis.readAllBytes();
+                } else {
+                    throw new InvalidZipArchiveException(
+                        "Zip entries do not match expected file names. Found file named " + zipEntry.getName()
+                    );
+                }
+            }
+            
+            if (Objects.isNull(signatureByteArray)) {
+                throw new InvalidZipArchiveException(
+                    "Invalid zip archive"
+                );
+            }
+            if (!signature.verify(signatureByteArray)) {
                 throw new DocSignatureFailureException(INVALID_SIGNATURE_MESSAGE);
             }
         } catch (SignatureException e) {
