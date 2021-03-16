f8471e6231ed87e9be52f503038e7c79e9b6a0e4
gauravchawla <gaurav-chawla@hotmail.com>
2021-03-16 00:40:54 +0000

    Repo changes

--- src/main/java/uk/gov/hmcts/reform/blobrouter/data/reports/ReportRepository.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/data/reports/ReportRepository.java
@@ -3,9 +3,11 @@ package uk.gov.hmcts.reform.blobrouter.data.reports;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+import uk.gov.hmcts.reform.blobrouter.model.out.reports.EnvelopeCountSummaryReportItem;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -15,13 +17,16 @@ public class ReportRepository {
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
     private final EnvelopeSummaryMapper mapper;
+    private final EnvelopeCountSummaryMapper summaryMapper;
 
     public ReportRepository(
         NamedParameterJdbcTemplate jdbcTemplate,
-        EnvelopeSummaryMapper mapper
+        EnvelopeSummaryMapper mapper,
+        EnvelopeCountSummaryMapper summaryMapper
     ) {
         this.jdbcTemplate = jdbcTemplate;
         this.mapper = mapper;
+        this.summaryMapper = summaryMapper;
     }
 
     public List<EnvelopeSummary> getEnvelopeSummary(Instant from, Instant to) {
@@ -38,4 +43,34 @@ public class ReportRepository {
             this.mapper
         );
     }
+
+    public List<EnvelopeCountSummaryReportItem> getReportFor(LocalDate date) {
+        return jdbcTemplate.query(
+            "SELECT\n"
+                + "  first_events.container AS container,\n"
+                + "  date(first_events.createdat) AS date,\n"
+                + "  count(*) AS received,\n"
+                + "  SUM(CASE WHEN rejection_events.id IS NOT NULL THEN 1 ELSE 0 END) AS rejected\n"
+                + "FROM (\n"
+                + "  SELECT DISTINCT on (container, zipfilename)\n"
+                + "    container, zipfilename, createdat\n"
+                + "  FROM process_events\n"
+                + "  ORDER BY container, zipfilename, createdat ASC\n"
+                + ") AS first_events\n"
+                + "LEFT JOIN (\n"
+                + "  SELECT DISTINCT on (container, zipfilename)\n"
+                + "    id, container, zipfilename\n"
+                + "  FROM process_events\n"
+                + "  WHERE event IN ('DOC_FAILURE', 'FILE_VALIDATION_FAILURE', 'DOC_SIGNATURE_FAILURE')\n"
+                + ") AS rejection_events\n"
+                + "ON rejection_events.container = first_events.container\n"
+                + "AND rejection_events.zipfilename = first_events.zipfilename\n"
+                + "GROUP BY first_events.container, date(first_events.createdat)\n"
+                + "HAVING date(first_events.createdat) = :date\n",
+            new MapSqlParameterSource()
+                .addValue("date", Instant.from(date)),
+            this.summaryMapper
+        );
+    }
+
 }
---
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/model/out/reports/EnvelopeCountSummaryReportItem.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.reform.blobrouter.model.out.reports;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+
+public class EnvelopeCountSummaryReportItem {
+    @JsonProperty("received")
+    public final int received;
+
+    @JsonProperty("rejected")
+    public final int rejected;
+
+    @JsonProperty("container")
+    public final String container;
+
+    @JsonProperty("date")
+    public final LocalDate date;
+
+    // region constructor
+    public EnvelopeCountSummaryReportItem(int received, int rejected, String container, LocalDate date) {
+        this.received = received;
+        this.rejected = rejected;
+        this.container = container;
+        this.date = date;
+    }
+    // endregion
+}
