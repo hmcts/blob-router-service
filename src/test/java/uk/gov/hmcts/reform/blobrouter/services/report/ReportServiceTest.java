24c9e18a1f9271e7b9a3595eed5dc948082f0c23
btensay <tensay.bulcha@gmail.com>
2021-03-25 09:40:47 +0000

    Report Service tests added
    Three test cases add for the Report Service.

--- src/test/java/uk/gov/hmcts/reform/blobrouter/services/report/ReportServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/blobrouter/services/report/ReportServiceTest.java
@@ -9,9 +9,11 @@ import uk.gov.hmcts.reform.blobrouter.config.ServiceConfiguration;
 import uk.gov.hmcts.reform.blobrouter.data.reports.EnvelopeSummary;
 import uk.gov.hmcts.reform.blobrouter.data.reports.ReportRepository;
 import uk.gov.hmcts.reform.blobrouter.model.out.EnvelopeSummaryItem;
+import uk.gov.hmcts.reform.blobrouter.model.out.reports.EnvelopeCountSummaryReportItem;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -28,6 +30,11 @@ import static uk.gov.hmcts.reform.blobrouter.util.DateTimeUtils.localTime;
 
 @ExtendWith(MockitoExtension.class)
 class ReportServiceTest {
+    private static final String TEST_CONTAINER = "bulkscan";
+    private static final String CRIME_CONTAINER = "crime";
+    private static final String PCQ_CONTAINER = "pcq";
+    private static final String PROBATE_CONTAINER = "probate";
+
     private ReportService reportService;
 
     @Mock
@@ -113,4 +120,107 @@ class ReportServiceTest {
                 )
             );
     }
+
+    @Test
+    void should_return_envelope_count_summary_list_for_matching_date() {
+        //given
+        LocalDate dateCountedFor = LocalDate.of(2020, 5, 22);
+        List<String> containerNames = Arrays.asList("crime", "pcq", "probate");
+
+        given(serviceConfiguration.getSourceContainers())
+            .willReturn(containerNames);
+
+        given(reportRepository.getReportFor(dateCountedFor, containerNames))
+            .willReturn(asList(
+                new EnvelopeCountSummaryReportItem(5, 2, CRIME_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(54, 12, PCQ_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(25, 8, PROBATE_CONTAINER, dateCountedFor)
+            ));
+        boolean includeTestContainer = false;
+
+        //when
+        List<EnvelopeCountSummaryReportItem> result = reportService.getCountFor(
+            dateCountedFor,
+            includeTestContainer
+        );
+
+        //then
+        assertThat(result)
+            .usingFieldByFieldElementComparator()
+            .containsExactly(
+                new EnvelopeCountSummaryReportItem(5, 2, CRIME_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(54, 12, PCQ_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(25, 8, PROBATE_CONTAINER, dateCountedFor)
+            );
+    }
+
+    @Test
+    void should_return_envelope_count_summary_list_for_matching_date_with_test_container() {
+        //given
+        LocalDate dateCountedFor = LocalDate.of(2019, 8, 14);
+        boolean includeTestContainer = true;
+        List<String> containerNames = Arrays.asList("crime", "pcq", "probate", TEST_CONTAINER);
+
+        given(serviceConfiguration.getSourceContainers())
+            .willReturn(containerNames);
+
+        given(reportRepository.getReportFor(dateCountedFor, containerNames))
+            .willReturn(asList(
+                new EnvelopeCountSummaryReportItem(5, 2, CRIME_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(54, 12, PCQ_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(25, 8, PROBATE_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(15, 6, TEST_CONTAINER, dateCountedFor)
+            ));
+
+        //when
+        List<EnvelopeCountSummaryReportItem> result = reportService.getCountFor(
+            dateCountedFor,
+            includeTestContainer
+        );
+
+        //then
+        assertThat(result)
+            .usingFieldByFieldElementComparator()
+            .containsExactly(
+                new EnvelopeCountSummaryReportItem(5, 2, CRIME_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(54, 12, PCQ_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(25, 8, PROBATE_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(15, 6, TEST_CONTAINER, dateCountedFor)
+            );
+    }
+
+    @Test
+    void should_return_envelope_count_summary_list_for_matching_date_with_out_test_container() {
+        //given
+        LocalDate dateCountedFor = LocalDate.of(2018, 2, 10);
+        boolean includeTestContainer = false;
+        List<String> containerNames = Arrays.asList("crime", "pcq", "probate", "bulkscan");
+
+        given(serviceConfiguration.getSourceContainers())
+            .willReturn(containerNames);
+
+        given(reportRepository.getReportFor(dateCountedFor, containerNames))
+            .willReturn(asList(
+                new EnvelopeCountSummaryReportItem(23, 5, CRIME_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(105, 28, PCQ_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(125, 18, PROBATE_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(135, 36, TEST_CONTAINER, dateCountedFor)
+            ));
+
+        //when
+        List<EnvelopeCountSummaryReportItem> result = reportService.getCountFor(
+            dateCountedFor,
+            includeTestContainer
+        );
+
+        //then
+        assertThat(result)
+            .usingFieldByFieldElementComparator()
+            .containsExactly(
+                new EnvelopeCountSummaryReportItem(23, 5, CRIME_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(105, 28, PCQ_CONTAINER, dateCountedFor),
+                new EnvelopeCountSummaryReportItem(125, 18, PROBATE_CONTAINER, dateCountedFor)
+            );
+    }
+
 }
