db8192f5e795a427ae03bfd04e6f067b442684de
Andrey Pereverzin <andrey.pereverzin@hmcts.net>
2021-06-01 18:57:58 +0100

    Removed unused method

--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/util/DateTimeUtils.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/util/DateTimeUtils.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.blobrouter.util;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 import static uk.gov.hmcts.reform.blobrouter.util.TimeZones.EUROPE_LONDON_ZONE_ID;
@@ -16,13 +15,5 @@ public final class DateTimeUtils {
 
     public static Instant instant(String string) {
         return LocalDateTime.parse(string, formatter).atZone(EUROPE_LONDON_ZONE_ID).toInstant();
-    }
-
-    public static String toLocalTimeZone(Instant instant) {
-        if (instant != null) {
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-            return dateTimeFormatter.format(ZonedDateTime.ofInstant(instant, EUROPE_LONDON_ZONE_ID));
-        }
-        return null;
     }
 }
