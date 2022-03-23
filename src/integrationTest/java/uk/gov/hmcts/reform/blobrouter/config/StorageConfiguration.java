e43b6463d2a926f2fc863b9b3d18c7439c1afe51
MUSTAFA <mustafa.dayican@hmcts.net>
2022-03-23 03:53:09 +0000

    revert

--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/config/StorageConfiguration.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/config/StorageConfiguration.java
@@ -1,4 +1,5 @@
 package uk.gov.hmcts.reform.blobrouter.config;
+
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
@@ -7,7 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-	@@ -15,10 +14,8 @@
+import static org.mockito.Mockito.mock;
+
+@Configuration
+public class StorageConfiguration {
 
     @Bean
     public StorageSharedKeyCredential getStorageSharedKeyCredential(
