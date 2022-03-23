957589ade08e7a33652df84060660b7917861a34
MUSTAFA <mustafa.dayican@hmcts.net>
2022-03-23 03:51:39 +0000

    revert

--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/config/StorageConfiguration.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/config/StorageConfiguration.java
@@ -1,21 +1,20 @@
 package uk.gov.hmcts.reform.blobrouter.config;
-
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.azure.storage.common.StorageSharedKeyCredential;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static org.mockito.Mockito.mock;
-
-@Configuration
-public class StorageConfiguration {
+	@@ -15,10 +14,8 @@
 
     @Bean
     public StorageSharedKeyCredential getStorageSharedKeyCredential(
+        @Value("${storage.account-name}") String accountName,
+        @Value("${storage.account-key}") String accountKey
     ) {
-        return new StorageSharedKeyCredential("bulkscan", "testkey");
+        return new StorageSharedKeyCredential(accountName, accountKey);
     }
 
     // not used as needs test context so it can actually be build
