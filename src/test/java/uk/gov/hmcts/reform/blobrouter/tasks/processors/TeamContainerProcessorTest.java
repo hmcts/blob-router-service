5f33210ffe15df0b41f102e82fd74f7f7631e4f9
Jason Paige <j@jasonpaige.co.uk>
2022-08-19 15:22:18 +0100

    test

--- src/test/java/uk/gov/hmcts/reform/blobrouter/tasks/processors/TeamContainerProcessorTest.java
+++ src/test/java/uk/gov/hmcts/reform/blobrouter/tasks/processors/TeamContainerProcessorTest.java
@@ -1,14 +1,28 @@
 package uk.gov.hmcts.reform.blobrouter.tasks.processors;
 
+import com.azure.core.http.rest.PagedFlux;
+import com.azure.core.http.rest.PagedIterable;
+import com.azure.core.http.rest.PagedResponse;
+import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.models.BlobItem;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import reactor.core.publisher.Mono;
 import uk.gov.hmcts.reform.blobrouter.services.BlobVerifier;
 import uk.gov.hmcts.reform.blobrouter.services.storage.LeaseAcquirer;
 
+import java.util.Iterator;
+import java.util.function.Function;
+
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 class TeamContainerProcessorTest {
 
@@ -23,11 +37,30 @@ class TeamContainerProcessorTest {
 
     @BeforeEach
     void setUp() {
+        storageClient = Mockito.mock(BlobServiceClient.class);
         teamContainerProcessor = new TeamContainerProcessor(storageClient, leaseAcquirer, blobVerifier);
     }
 
     @Test
+    @SuppressWarnings("unchecked")
     void should_return_an_empty_list() {
+        var blobContainerClient = Mockito.mock(BlobContainerClient.class);
+        given(storageClient.getBlobContainerClient(anyString()))
+            .willReturn(blobContainerClient);
+        given(blobContainerClient.generateSas(any()))
+            .willReturn("testSas");
+
+        Iterator<BlobItem> mockIterator = (Iterator<BlobItem>) Mockito.mock(Iterator.class);
+        given(mockIterator.hasNext()).willReturn(true, false);
+        given(mockIterator.next()).willReturn(Mockito.mock(BlobItem.class));
+
+        PagedIterable<BlobItem> mockPagedBlobItems = (PagedIterable<BlobItem>) Mockito.mock(PagedIterable.class);
+
+        given(mockPagedBlobItems.iterator()).willReturn(mockIterator);
+
+        given(blobContainerClient.listBlobs())
+            .willReturn(mockPagedBlobItems);
+
         var envelopes = teamContainerProcessor.leaseAndGetEnvelopes("nfd");
 
         assertThat(envelopes).isEqualTo(emptyList());
