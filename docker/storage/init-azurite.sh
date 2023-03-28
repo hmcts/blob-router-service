9dbb26cf0b350de36e7a725c85db5b2e29e77cd9
Adam Stevenson <addstevenson@hotmail.com>
2023-03-28 11:51:42 +0100

    testing localenv (#1997)

---
+++ docker/storage/init-azurite.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -e
+
+SOURCE_CONNECTION_STRING="DefaultEndpointsProtocol=http;AccountName=reformscanlocal;AccountKey=cmVmb3Jtc2NhbmtleQo=;BlobEndpoint=http://azure-storage-emulator-azurite:10000/reformscanlocal;"
+
+az storage container create --name pcq --connection-string $SOURCE_CONNECTION_STRING
+az storage container create --name crime --connection-string $SOURCE_CONNECTION_STRING
+az storage container create --name bulkscan --connection-string $SOURCE_CONNECTION_STRING
+az storage container create --name bulkscanauto --connection-string $SOURCE_CONNECTION_STRING
+az storage container create --name publiclaw --connection-string $SOURCE_CONNECTION_STRING
+az storage container create --name sscs --connection-string $SOURCE_CONNECTION_STRING
+az storage container create --name nfd --connection-string $SOURCE_CONNECTION_STRING
+az storage container create --name finrem --connection-string $SOURCE_CONNECTION_STRING
+az storage container create --name divorce --connection-string $SOURCE_CONNECTION_STRING
+az storage container create --name probate --connection-string $SOURCE_CONNECTION_STRING
+az storage container create --name cmc --connection-string $SOURCE_CONNECTION_STRING
+az storage container create --name mosh --connection-string $SOURCE_CONNECTION_STRING
+az storage container create --name jason --connection-string $SOURCE_CONNECTION_STRING
+
+SOURCE_CONNECTION_STRING2="DefaultEndpointsProtocol=http;AccountName=bulkscanlocal;AccountKey=cmVmb3Jtc2NhbmtleQo=;BlobEndpoint=http://azure-storage-emulator-azurite:10000/bulkscanlocal;"
+
+az storage container create --name pcq --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name pcq-rejected --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name crime --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name crime-rejected --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name bulkscan --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name bulkscan-rejected --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name bulkscanauto --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name bulkscanauto-rejected --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name publiclaw --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name publiclaw-rejected --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name sscs --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name sscs-rejected --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name nfd --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name nfd-rejected --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name finrem --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name finrem-rejected --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name divorce --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name divorce-rejected --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name probate --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name probate-rejected --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name cmc --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name cmc-rejected --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name mosh --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name mosh-rejected --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name jason --connection-string $SOURCE_CONNECTION_STRING2
+az storage container create --name jason-rejected --connection-string $SOURCE_CONNECTION_STRING2
+
+
