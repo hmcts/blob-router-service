048edb2cfb8aba6876cac2323529ab2487b22dcb
amstevenson <addstevenson@hotmail.com>
2023-11-07 11:45:58 +0000

    trying out a retry

--- src/functionalTest/java/uk/gov/hmcts/reform/blobrouter/RejectedEnvelopesNotificationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/blobrouter/RejectedEnvelopesNotificationTest.java
@@ -11,9 +11,6 @@ import java.util.concurrent.TimeUnit;
 
 import static com.jayway.awaitility.Awaitility.await;
 import static java.util.Collections.singletonList;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItems;
-import static org.springframework.http.HttpStatus.OK;
 import static uk.gov.hmcts.reform.blobrouter.data.envelopes.Status.REJECTED;
 import static uk.gov.hmcts.reform.blobrouter.envelope.ZipFileHelper.createZipArchive;
 import static uk.gov.hmcts.reform.blobrouter.storage.StorageHelper.blobExists;
@@ -47,21 +44,22 @@ public class RejectedEnvelopesNotificationTest extends FunctionalTestBase {
     }
 
     private void assertNotificationIsSent(String fileName) {
-//        RestAssured
-//            .given()
-//            .baseUri(config.blobRouterUrl)
-//            .relaxedHTTPSValidation()
-//            .queryParam("file_name", fileName)
-//            .queryParam("container", BULK_SCAN_CONTAINER)
-//            .get("/envelopes")
-//            .then()
-//            .statusCode(OK.value())
-//            .body("data[0].status", equalTo(REJECTED.name()))
-//            .body("data[0].pending_notification", equalTo(false))
-//            .body(
-//                "data[0].events.event", hasItems(EventType.REJECTED.name(), EventType.NOTIFICATION_SENT.name())
-//            );
-
+        //        RestAssured
+        //            .given()
+        //            .baseUri(config.blobRouterUrl)
+        //            .relaxedHTTPSValidation()
+        //            .queryParam("file_name", fileName)
+        //            .queryParam("container", BULK_SCAN_CONTAINER)
+        //            .get("/envelopes")
+        //            .then()
+        //            .statusCode(OK.value())
+        //            .body("data[0].status", equalTo(REJECTED.name()))
+        //            .body("data[0].pending_notification", equalTo(false))
+        //            .body(
+        //                "data[0].events.event",
+        //                hasItems(EventType.REJECTED.name(),
+        //                EventType.NOTIFICATION_SENT.name())
+        //            );
 
         Response response = RestAssured
             .given()
@@ -74,17 +72,19 @@ public class RejectedEnvelopesNotificationTest extends FunctionalTestBase {
         await()
             .atMost(30, TimeUnit.SECONDS) // Adjust the time duration as needed
             .until(() -> {
+                boolean isResponse200 = response.getStatusCode() == 200;
                 String responseData = response.getBody().asString();
                 String pendingNotificationValue = response.then().extract().path("data[0].pending_notification");
                 String statusValue = response.then().extract().path("data[0].status");
                 List<String> events = response.then().extract().path("data[0].events.event");
 
                 // Replace the expected values with the ones you want to wait for
-                return responseData != null &&
-                    "false".equals(pendingNotificationValue) &&
-                    REJECTED.name().equals(statusValue) &&
-                    events.contains(EventType.REJECTED.name()) &&
-                    events.contains(EventType.NOTIFICATION_SENT.name());
+                return isResponse200
+                    && responseData != null
+                    && "false".equals(pendingNotificationValue)
+                    && REJECTED.name().equals(statusValue)
+                    && events.contains(EventType.REJECTED.name())
+                    && events.contains(EventType.NOTIFICATION_SENT.name());
             });
     }
 }
