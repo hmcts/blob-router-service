32a767a07b3ad97c2b62f8ab87ad03dd67c1cf73
amstevenson <addstevenson@hotmail.com>
2023-11-07 12:03:04 +0000

    checking if waiting for a bit works

--- src/functionalTest/java/uk/gov/hmcts/reform/blobrouter/RejectedEnvelopesNotificationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/blobrouter/RejectedEnvelopesNotificationTest.java
@@ -1,16 +1,17 @@
 package uk.gov.hmcts.reform.blobrouter;
 
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.blobrouter.data.events.EventType;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static com.jayway.awaitility.Awaitility.await;
 import static java.util.Collections.singletonList;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import static org.springframework.http.HttpStatus.OK;
 import static uk.gov.hmcts.reform.blobrouter.data.envelopes.Status.REJECTED;
 import static uk.gov.hmcts.reform.blobrouter.envelope.ZipFileHelper.createZipArchive;
 import static uk.gov.hmcts.reform.blobrouter.storage.StorageHelper.blobExists;
@@ -43,48 +44,21 @@ public class RejectedEnvelopesNotificationTest extends FunctionalTestBase {
         assertNotificationIsSent(fileName);
     }
 
-    private void assertNotificationIsSent(String fileName) {
-        //        RestAssured
-        //            .given()
-        //            .baseUri(config.blobRouterUrl)
-        //            .relaxedHTTPSValidation()
-        //            .queryParam("file_name", fileName)
-        //            .queryParam("container", BULK_SCAN_CONTAINER)
-        //            .get("/envelopes")
-        //            .then()
-        //            .statusCode(OK.value())
-        //            .body("data[0].status", equalTo(REJECTED.name()))
-        //            .body("data[0].pending_notification", equalTo(false))
-        //            .body(
-        //                "data[0].events.event",
-        //                hasItems(EventType.REJECTED.name(),
-        //                EventType.NOTIFICATION_SENT.name())
-        //            );
-
-        Response response = RestAssured
+    private void assertNotificationIsSent(String fileName) throws InterruptedException {
+        Thread.sleep(5000);
+        RestAssured
             .given()
             .baseUri(config.blobRouterUrl)
             .relaxedHTTPSValidation()
             .queryParam("file_name", fileName)
             .queryParam("container", BULK_SCAN_CONTAINER)
-            .get("/envelopes");
-
-        await()
-            .atMost(30, TimeUnit.SECONDS) // Adjust the time duration as needed
-            .until(() -> {
-                boolean isResponse200 = response.getStatusCode() == 200;
-                String responseData = response.getBody().asString();
-                String pendingNotificationValue = response.then().extract().path("data[0].pending_notification");
-                String statusValue = response.then().extract().path("data[0].status");
-                List<String> events = response.then().extract().path("data[0].events.event");
-
-                // Replace the expected values with the ones you want to wait for
-                return isResponse200
-                    && responseData != null
-                    && "false".equals(pendingNotificationValue)
-                    && REJECTED.name().equals(statusValue)
-                    && events.contains(EventType.REJECTED.name())
-                    && events.contains(EventType.NOTIFICATION_SENT.name());
-            });
+            .get("/envelopes")
+            .then()
+            .statusCode(OK.value())
+            .body("data[0].status", equalTo(REJECTED.name()))
+            .body("data[0].pending_notification", equalTo(false))
+            .body(
+                "data[0].events.event", hasItems(EventType.REJECTED.name(), EventType.NOTIFICATION_SENT.name())
+            );
     }
 }
