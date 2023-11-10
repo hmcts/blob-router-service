333b0b27aa7490c8184847992593c46a35d37e1e
Adam Stevenson <addstevenson@hotmail.com>
2023-11-10 11:26:59 +0000

    Fact 1514 staging (#2130)
    * adding flex server secrets
    
    * adding renovate
    
    * fixing cves
    
    * fixing cves
    
    * adding sup
    
    * adding sup
    
    * adding sup
    
    * tf error
    
    * updating java v
    
    * update servicebus version chart.yaml
    
    * editing func test
    
    * trying out a retry
    
    * checking if waiting for a bit works
    
    * waiting a little bit longer for the notification to get consumed
    
    * fixing func test
    
    * removing assertion as its dependent on service, so can be removed
    
    * check if giving service time helps
    
    * printing out to debug
    
    * printing out to debug
    
    * trying out change to namespace
    
    * trying out change to namespace
    
    * trying out change to namespace
    
    * trying out change to namespace
    
    * fixing inf values
    
    * fixing inf values
    
    * adding staging db
    
    * false positive
    
    * splitting into sep file
    
    * allow removal of ss data
    
    * revert
    
    * modifying suppr
    
    * changes
    
    ---------
    
    Co-authored-by: Imran Ali <Imran.Ali@justice.gov.uk>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -20,30 +20,6 @@ locals {
   postgresql_user = "api_user"
 }
 
-module "postgresql" {
-  providers = {
-    azurerm.postgres_network = azurerm.postgres_network
-  }
-
-  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
-  name                 = local.db_host_name
-  product              = var.product
-  component            = var.component
-  location             = var.location
-  env                  = var.env
-  pgsql_admin_username = local.postgresql_user
-  pgsql_databases = [
-    {
-      name : local.db_name
-    }
-  ]
-  common_tags   = var.common_tags
-  business_area = "cft"
-  pgsql_version = "15"
-
-  admin_user_object_id = var.jenkins_AAD_objectId
-}
-
 module "reform-blob-router-db" {
   source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
   product            = var.product
---
+++ infrastructure/tf-db-flexible.tf
@@ -0,0 +1,47 @@
+module "postgresql" {
+  providers = {
+    azurerm.postgres_network = azurerm.postgres_network
+  }
+
+  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  name                 = local.db_host_name
+  product              = var.product
+  component            = var.component
+  location             = var.location
+  env                  = var.env
+  pgsql_admin_username = local.postgresql_user
+  pgsql_databases = [
+    {
+      name : local.db_name
+    }
+  ]
+  common_tags   = var.common_tags
+  business_area = "cft"
+  pgsql_version = "15"
+
+  admin_user_object_id = var.jenkins_AAD_objectId
+}
+
+module "postgresql-staging" {
+  providers = {
+    azurerm.postgres_network = azurerm.postgres_network
+  }
+
+  source               = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  name                 = local.db_host_name
+  product              = "${var.component}-staging"
+  component            = var.component
+  location             = var.location
+  env                  = "aat"
+  pgsql_admin_username = "blob_router"
+  pgsql_databases = [
+    {
+      name : local.db_name
+    }
+  ]
+  common_tags   = var.common_tags
+  business_area = "cft"
+  pgsql_version = "15"
+
+  admin_user_object_id = var.jenkins_AAD_objectId
+}
