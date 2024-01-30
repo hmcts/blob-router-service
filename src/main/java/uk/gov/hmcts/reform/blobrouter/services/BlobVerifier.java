37272f491795a8b7f4e338e8ed5d239c8767f452
Josh Blackmoor <joshblackmoor01@icloud.com>
2024-01-30 13:20:44 +0000

    First test code

--- src/main/java/uk/gov/hmcts/reform/blobrouter/services/BlobVerifier.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/services/BlobVerifier.java
@@ -13,6 +13,7 @@ import uk.gov.hmcts.reform.blobrouter.util.zipverification.ZipVerifiers;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.PublicKey;
+import java.util.List;
 import java.util.zip.ZipInputStream;
 
 import static com.google.common.io.Resources.getResource;
@@ -31,33 +32,46 @@ public class BlobVerifier {
     private static final VerificationResult INVALID_ZIP_ARCHIVE_VERIFICATION_RESULT =
             getError(ErrorCode.ERR_ZIP_PROCESSING_FAILED, "Invalid zip archive");
 
-    private final PublicKey publicKey;
+    private final PublicKey excelaPublicKey;
+    private final PublicKey ironMountainPublicKey;
 
     public BlobVerifier(
-        @Value("${public-key-der-file}") String publicKeyDerFilename
+        @Value("${public-key-der-file}") String excelaPublicKeyDerFilename,
+        @Value("${public-key-der-file}") String ironMountainPublicKeyDerFilename
     ) {
         try {
-            this.publicKey = PublicKeyDecoder.decode(toByteArray(getResource(publicKeyDerFilename)));
+            this.excelaPublicKey = PublicKeyDecoder.decode(toByteArray(getResource(excelaPublicKeyDerFilename)));
         } catch (Exception e) {
-            throw new InvalidConfigException("Error loading public key. File name: " + publicKeyDerFilename, e);
+            throw new InvalidConfigException("Error loading Excela public key. File name: "
+                                                 + excelaPublicKeyDerFilename, e);
+        }
+
+        try {
+            this.ironMountainPublicKey = PublicKeyDecoder
+                .decode(toByteArray(getResource(ironMountainPublicKeyDerFilename)));
+        } catch (Exception e) {
+            throw new InvalidConfigException("Error loading Iron Mountain public key. File name: "
+                                                 + ironMountainPublicKeyDerFilename, e);
         }
     }
 
     public VerificationResult verifyZip(String blobName, InputStream zipSource) {
-        try (var zis = new ZipInputStream(zipSource)) {
-
-            ZipVerifiers.verifyZip(zis, publicKey);
-            return OK_VERIFICATION_RESULT;
-        } catch (DocSignatureFailureException ex) {
-            logger.info("Invalid signature. Blob name: {}", blobName, ex);
-            return INVALID_SIGNATURE_VERIFICATION_RESULT;
-        } catch (InvalidZipArchiveException ex) {
-            logger.info("Invalid zip archive. Blob name: {}", blobName, ex);
-            return INVALID_ZIP_ARCHIVE_VERIFICATION_RESULT;
-        } catch (IOException ex) {
-            logger.info("Error occurred when verifying file. Blob name: {}", blobName, ex);
-            return INVALID_ZIP_ARCHIVE_VERIFICATION_RESULT;
+        List<PublicKey> publicKeyList = List.of(excelaPublicKey, ironMountainPublicKey);
+        for (PublicKey publicKey : publicKeyList) {
+            try (var zis = new ZipInputStream(zipSource)) {
+                ZipVerifiers.verifyZip(zis, publicKey);
+                return OK_VERIFICATION_RESULT;
+            } catch (DocSignatureFailureException ex) {
+                logger.info("Invalid signature using public key {}. Blob name: {}", publicKey, blobName, ex);
+            } catch (InvalidZipArchiveException ex) {
+                logger.info("Invalid zip archive. Blob name: {}", blobName, ex);
+                return INVALID_ZIP_ARCHIVE_VERIFICATION_RESULT;
+            } catch (IOException ex) {
+                logger.info("Error occurred when verifying file. Blob name: {}", blobName, ex);
+                return INVALID_ZIP_ARCHIVE_VERIFICATION_RESULT;
+            }
         }
+        return INVALID_SIGNATURE_VERIFICATION_RESULT;
     }
 
     public static class VerificationResult {
@@ -65,7 +79,8 @@ public class BlobVerifier {
         public final ErrorCode error;
         public final String errorDescription;
 
-        public static final VerificationResult OK_VERIFICATION_RESULT = new VerificationResult(true, null, null);
+        public static final VerificationResult OK_VERIFICATION_RESULT = new VerificationResult(
+            true,null, null);
 
         private VerificationResult(boolean isOk, ErrorCode error, String errorDescription) {
             this.isOk = isOk;
