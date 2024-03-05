0763326133a259e605f5cf9f7cb7eb6cea9239d5
amstevenson <addstevenson@hotmail.com>
2024-03-05 18:14:59 +0000

    changes, hopefully int tests pass

--- src/main/java/uk/gov/hmcts/reform/blobrouter/Application.java
+++ src/main/java/uk/gov/hmcts/reform/blobrouter/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients(basePackages = {"uk.gov.hmcts.reform"})
-@SpringBootApplication(proxyBeanMethods = false)
+@SpringBootApplication
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
