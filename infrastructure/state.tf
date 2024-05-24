b2678f9ffe63616d6fbdaa241ea4dda76c88c961
Ruth Bovell <ruth.bovell@justice.gov.uk>
2024-05-24 10:02:40 +0100

    FACT-1698 Renovate Dependency Updates (#2198)
    * Update Helm release java to v5.0.2
    
    * Bumping chart version/ fixing aliases
    
    * Update Terraform azuread to v2.47.0
    
    * Update dependency hashicorp/terraform to v1.7.3
    
    * Update Terraform azurerm to v3.92.0
    
    * Update dependency gradle to v7.6.4
    
    * Update dependency org.postgresql:postgresql to v42.7.3
    
    * Update dependency com.azure:azure-storage-blob to v12.25.3
    
    * Update dependency org.flywaydb:flyway-core to v10
    
    * Update plugin org.flywaydb.flyway to v10
    
    * Update Terraform azuread to v2.48.0
    
    * Update Terraform azurerm to v3.99.0
    
    * Bumping chart version/ fixing aliases
    
    * - rollback azure-storage-blob due to breaking changes
    
    * - attempt fix for upgrading to flyway-core 10.x
    
    * revert "Merge remote-tracking branch 'origin/renovate/gradle-7.x' into FACT-1698-renovate-updates"
    
    This reverts commit 1d3970eb61bdd342eeefa93d655cf896d3df96cf, reversing
    changes made to b4a3a4e840bb0a4342fe8f354f269d8ad5bcfefc.
    
    * - manual gradle version upgrade
    
    * - removes not needed suppressions
    
    * - puts back azure suppression
    
    * - manually updates spring boot and springdoc to bring in newer jackson-databind
    
    * - suppress jackson-databind cve
    
    * - removes extra suppression brought in from merging in master
    
    * - adds flyway and postgres to buildscript block
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: hmcts-jenkins-a-to-c <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.84.0"
+      version = "3.99.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.46.0"
+      version = "2.48.0"
     }
     random = {
       source = "hashicorp/random"
