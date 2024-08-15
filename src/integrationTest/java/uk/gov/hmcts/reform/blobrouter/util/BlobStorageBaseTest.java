86ae886fb85686ec80926259dd197b170b74a275
Ruth Bovell <ruthb111@outlook.com>
2024-08-15 14:07:40 +0100

    - lower version of azurite and storage blob

--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/util/BlobStorageBaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/util/BlobStorageBaseTest.java
@@ -16,7 +16,7 @@ import java.util.Set;
 public abstract class BlobStorageBaseTest {
 
     private static GenericContainer<?> dockerComposeContainer =
-        new GenericContainer<>("hmctspublic.azurecr.io/imported/azure-storage/azurite:3.31.0")
+        new GenericContainer<>("hmctspublic.azurecr.io/imported/azure-storage/azurite:3.29.0")
         .withExposedPorts(10000);
     protected static BlobServiceClient storageClient;
     private static String dockerHost;
