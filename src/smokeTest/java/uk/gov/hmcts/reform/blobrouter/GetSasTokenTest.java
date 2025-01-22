9a4834ef208478710e42b94a00169a6d960caebc
Ruth Bovell <ruthb111@outlook.com>
2025-01-22 15:57:43 +0000

    adds new smoke tests for new APIM

--- src/smokeTest/java/uk/gov/hmcts/reform/blobrouter/GetSasTokenTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/blobrouter/GetSasTokenTest.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.blobrouter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -11,7 +13,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
-
 public class GetSasTokenTest extends ApiGatewayBaseTest {
 
     private static final String SAS_TOKEN_ENDPOINT_PATH = "/token/bulkscan";
@@ -21,6 +22,7 @@ public class GetSasTokenTest extends ApiGatewayBaseTest {
         loadConfig();
     }
 
+    //APIM Certificate Tests (OLD)
     @Disabled
     @Test
     void should_accept_request_with_valid_certificate_and_subscription_key() throws Exception {
@@ -58,6 +60,31 @@ public class GetSasTokenTest extends ApiGatewayBaseTest {
         assertThat(response.body().asString()).contains("Access denied due to missing subscription key");
     }
 
+    //APIM OAUTH Tests
+    @Test
+    void should_reject_request_with_missing_jwt_token() throws Exception {
+        Response response = callSasTokenEndpointWithoutJwt();
+
+        assertThat(response.statusCode()).isEqualTo(UNAUTHORIZED.value());
+        assertThat(response.body().asString()).contains("Unauthorized. Access token is missing or invalid.");
+    }
+
+    @Test
+    void should_reject_request_with_invalid_jwt_token() throws Exception {
+        Response response = callSasTokenEndpointWithJwt("imnotarealaccesstoken");
+        assertThat(response.statusCode()).isEqualTo(UNAUTHORIZED.value());
+        assertThat(response.body().asString()).contains("Unauthorized. Access token is missing or invalid.");
+    }
+
+    @Test
+    void should_accept_request_with_valid_jwt_token() throws Exception {
+        String jwtAccessToken = getJwtAccessToken();
+        assertThat(jwtAccessToken).isNotEmpty();
+
+        Response response = callSasTokenEndpointWithJwt(jwtAccessToken);
+        assertThat(response.getStatusCode()).isEqualTo(OK.value());
+        assertThat(response.body().jsonPath().getString("sas_token")).isNotEmpty();
+    }
 
     private Response callSasTokenEndpoint(
         KeyStoreWithPassword clientKeyStore,
@@ -81,4 +108,19 @@ public class GetSasTokenTest extends ApiGatewayBaseTest {
         return request.get(SAS_TOKEN_ENDPOINT_PATH);
     }
 
+    private Response callSasTokenEndpointWithJwt(String jwtAccessToken) throws JsonProcessingException {
+        Response response = RestAssured.given().header("Authorization", jwtAccessToken)
+            .get(apiGatewayUrlNew + SAS_TOKEN_ENDPOINT_PATH)
+            .thenReturn();
+        System.out.println(response.asPrettyString());
+        return response;
+    }
+
+    private Response callSasTokenEndpointWithoutJwt() {
+        Response response = RestAssured.given()
+            .get(apiGatewayUrlNew + SAS_TOKEN_ENDPOINT_PATH)
+            .thenReturn();
+        System.out.println(response.asPrettyString());
+        return response;
+    }
 }
