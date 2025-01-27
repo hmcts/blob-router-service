535fccabfbc163010bad99c3b1cf52236675fb0b
Ruth Bovell <ruthb111@outlook.com>
2025-01-27 13:40:58 +0000

    - adds api gateway tests for reconciliation - clean up tests for getting sas token

--- src/apiGatewayTest/java/uk/gov/hmcts/reform/blobrouter/GetSasTokenTest.java
+++ src/apiGatewayTest/java/uk/gov/hmcts/reform/blobrouter/GetSasTokenTest.java
@@ -2,36 +2,17 @@ package uk.gov.hmcts.reform.blobrouter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @ExtendWith(SpringExtension.class)
-public class GetSasTokenTest {
-
-    @Value("${oauth-api-gateway-url}")
-    private String oauthApiGatewayUrl;
-
-    @Value("${bulk-scan-app-id}")
-    private String bulkScanAppId;
-
-    @Value("${bulk-scan-app-secret}")
-    private String bulkScanSecret;
-
-    @Value("${bulk-scan-client-id}")
-    private String bulkScanClientId;
-
-    @Value("${bulk-scan-client-secret}")
-    private String bulkScanClientSecret;
-
-    @Value("${tenant-id}")
-    private String tenantId;
+public class GetSasTokenTest extends OauthBase{
 
     private static final String SAS_TOKEN_ENDPOINT_PATH = "/token/bulkscan";
 
@@ -41,37 +22,34 @@ public class GetSasTokenTest {
         assertThat(jwtAccessToken).isNotEmpty();
 
         Response response = callSasTokenEndpointWithJwt(jwtAccessToken);
-        assertThat(response.getStatusCode()).isEqualTo(UNPROCESSABLE_ENTITY.value());
+        assertThat(response.getStatusCode()).isEqualTo(OK.value());
         assertThat(response.body().jsonPath().getString("sas_token")).isNotEmpty();
     }
 
-    protected String getJwtAccessToken() {
-        Response response = RestAssured
-            .given()
-            .contentType(ContentType.URLENC)
-            .formParam("grant_type", "client_credentials")
-            .formParam("client_id", bulkScanClientId)
-            .formParam("scope", "api://" + bulkScanAppId + "/.default")
-            .formParam("client_secret", bulkScanClientSecret)
-            .post("https://login.microsoftonline.com/" + tenantId + "/oauth2/v2.0/token")
-            .thenReturn();
+    @Test
+    void should_not_accept_request_without_valid_jwt_token() throws Exception {
+        Response response = callSasTokenEndpointWithoutJwt();
 
-        return response.jsonPath().getString("access_token");
+        assertThat(response.getStatusCode()).isEqualTo(UNAUTHORIZED.value());
+        assertThat(response.body().asString()).contains("Unauthorized. Access token is missing or invalid.");
+    }
+
+    @Test
+    void should_reject_request_with_invalid_jwt_token() throws Exception {
+        Response response = callSasTokenEndpointWithJwt("imnotarealaccesstoken");
+        assertThat(response.statusCode()).isEqualTo(UNAUTHORIZED.value());
+        assertThat(response.body().asString()).contains("Unauthorized. Access token is missing or invalid.");
     }
 
     private Response callSasTokenEndpointWithJwt(String jwtAccessToken) throws JsonProcessingException {
-        Response response = RestAssured.given().header("Authorization", jwtAccessToken)
-            .get(oauthApiGatewayUrl + SAS_TOKEN_ENDPOINT_PATH)
+        return RestAssured.given().header("Authorization", jwtAccessToken)
+            .get(getOauthApiGatewayUrl() + SAS_TOKEN_ENDPOINT_PATH)
             .thenReturn();
-        System.out.println(response.asPrettyString());
-        return response;
     }
 
     private Response callSasTokenEndpointWithoutJwt() {
-        Response response = RestAssured.given()
-            .get(oauthApiGatewayUrl + SAS_TOKEN_ENDPOINT_PATH)
+        return RestAssured.given()
+            .get(getOauthApiGatewayUrl() + SAS_TOKEN_ENDPOINT_PATH)
             .thenReturn();
-        System.out.println(response.asPrettyString());
-        return response;
     }
 }
---
+++ src/apiGatewayTest/java/uk/gov/hmcts/reform/blobrouter/ReconciliationApiTest.java
@@ -0,0 +1,62 @@
+package uk.gov.hmcts.reform.blobrouter;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+@ExtendWith(SpringExtension.class)
+public class ReconciliationApiTest extends OauthBase {
+
+    private static final String RECONCILIATION_ENDPOINT_PATH = "/reconciliation-report/{date}";
+    @Test
+    void should_reject_request_with_missing_jwt_token() {
+        Response response = callReconciliationEndpointWithoutJwt();
+
+        assertThat(response.statusCode()).isEqualTo(UNAUTHORIZED.value());
+        assertThat(response.body().asString()).contains("Unauthorized. Access token is missing or invalid.");
+    }
+
+    @Test
+    void should_reject_request_with_invalid_jwt_token() {
+        Response response = callReconciliationEndpointWithJwt("imnotarealaccesstoken");
+        assertThat(response.statusCode()).isEqualTo(UNAUTHORIZED.value());
+        assertThat(response.body().asString()).contains("Unauthorized. Access token is missing or invalid.");
+    }
+
+    @Test
+    void should_accept_request_with_valid_jwt_token() {
+        String jwtAccessToken = getJwtAccessToken();
+        assertThat(jwtAccessToken).isNotEmpty();
+
+        Response response = callReconciliationEndpointWithJwt(jwtAccessToken);
+        assertThat(response.getStatusCode()).isEqualTo(BAD_REQUEST.value());
+    }
+
+    private Response callReconciliationEndpointWithJwt(String jwtAccessToken) {
+        String statementsReport = "{\"test\": {}}";
+        return RestAssured.given().header("Authorization", jwtAccessToken)
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .body(statementsReport)
+            .post(getOauthApiGatewayUrl() + RECONCILIATION_ENDPOINT_PATH, LocalDate.now().toString())
+            .thenReturn();
+    }
+
+    private Response callReconciliationEndpointWithoutJwt() {
+        String statementsReport = "{\"test\": {}}";
+        return RestAssured.given()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .body(statementsReport)
+            .post(getOauthApiGatewayUrl() + RECONCILIATION_ENDPOINT_PATH, LocalDate.now().toString())
+            .thenReturn();
+    }
+}
