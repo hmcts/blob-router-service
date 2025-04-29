3231a400a6f7bd4fb56a1d47bf9fbb675c000236
josh-uk <joshua.blackmoor@justice.gov.uk>
2025-04-29 11:46:07 +0100

    Removed old apim and updated tests

--- src/smokeTest/java/uk/gov/hmcts/reform/blobrouter/GetSasTokenTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/blobrouter/GetSasTokenTest.java
@@ -1,11 +1,8 @@
 package uk.gov.hmcts.reform.blobrouter;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,51 +14,12 @@ public class GetSasTokenTest extends ApiGatewayBaseTest {
     private static final String SAS_TOKEN_ENDPOINT_PATH = "/token/bulkscan";
 
     @BeforeAll
-    static void setup() throws Exception {
+    static void setup() {
         loadConfig();
     }
 
-    //APIM Certificate Tests (OLD)
-    @Disabled
     @Test
-    void should_accept_request_with_valid_certificate_and_subscription_key() throws Exception {
-        Response response =
-            callSasTokenEndpoint(validClientKeyStore, validSubscriptionKey)
-                .thenReturn();
-
-        assertThat(response.getStatusCode()).isEqualTo(OK.value());
-        assertThat(response.body().jsonPath().getString("sas_token")).isNotEmpty();
-    }
-
-    @Disabled
-    @Test
-    void should_reject_request_with_invalid_subscription_key() throws Exception {
-        Response response = callSasTokenEndpoint(
-            validClientKeyStore,
-            "invalid-subscription-key123"
-        )
-            .thenReturn();
-
-        assertThat(response.statusCode()).isEqualTo(UNAUTHORIZED.value());
-        assertThat(response.body().asString()).contains("Access denied due to invalid subscription key");
-    }
-
-    @Disabled
-    @Test
-    void should_reject_request_lacking_subscription_key() throws Exception {
-        Response response = callSasTokenEndpoint(
-            validClientKeyStore,
-            null
-        )
-            .thenReturn();
-
-        assertThat(response.statusCode()).isEqualTo(UNAUTHORIZED.value());
-        assertThat(response.body().asString()).contains("Access denied due to missing subscription key");
-    }
-
-    //APIM OAUTH Tests
-    @Test
-    void should_reject_request_with_missing_jwt_token() throws Exception {
+    void should_reject_request_with_missing_jwt_token() {
         Response response = callSasTokenEndpointWithoutJwt();
 
         assertThat(response.statusCode()).isEqualTo(UNAUTHORIZED.value());
@@ -69,14 +27,14 @@ public class GetSasTokenTest extends ApiGatewayBaseTest {
     }
 
     @Test
-    void should_reject_request_with_invalid_jwt_token() throws Exception {
+    void should_reject_request_with_invalid_jwt_token() {
         Response response = callSasTokenEndpointWithJwt("imnotarealaccesstoken");
         assertThat(response.statusCode()).isEqualTo(UNAUTHORIZED.value());
         assertThat(response.body().asString()).contains("Unauthorized. Access token is missing or invalid.");
     }
 
     @Test
-    void should_accept_request_with_valid_jwt_token() throws Exception {
+    void should_accept_request_with_valid_jwt_token() {
         String jwtAccessToken = getJwtAccessToken();
         assertThat(jwtAccessToken).isNotEmpty();
 
@@ -85,29 +43,7 @@ public class GetSasTokenTest extends ApiGatewayBaseTest {
         assertThat(response.body().jsonPath().getString("sas_token")).isNotEmpty();
     }
 
-    private Response callSasTokenEndpoint(
-        KeyStoreWithPassword clientKeyStore,
-        String subscriptionKey
-    ) throws Exception {
-        RequestSpecification request = RestAssured.given().baseUri(apiGatewayUrl);
-
-        if (clientKeyStore != null) {
-            request = request.config(
-                getSslConfigForClientCertificate(
-                    clientKeyStore.keyStore,
-                    clientKeyStore.password
-                )
-            );
-        }
-
-        if (subscriptionKey != null) {
-            request = request.header(SUBSCRIPTION_KEY_HEADER_NAME, subscriptionKey);
-        }
-
-        return request.get(SAS_TOKEN_ENDPOINT_PATH);
-    }
-
-    private Response callSasTokenEndpointWithJwt(String jwtAccessToken) throws JsonProcessingException {
+    private Response callSasTokenEndpointWithJwt(String jwtAccessToken) {
         Response response = RestAssured.given().header("Authorization", jwtAccessToken)
             .get(oauthGatewayUrl + SAS_TOKEN_ENDPOINT_PATH)
             .thenReturn();
--- src/smokeTest/java/uk/gov/hmcts/reform/blobrouter/ReconciliationApiTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/blobrouter/ReconciliationApiTest.java
@@ -2,13 +2,10 @@ package uk.gov.hmcts.reform.blobrouter;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import uk.gov.hmcts.reform.logging.appinsights.SyntheticHeaders;
 
 import java.time.LocalDate;
 
@@ -21,38 +18,10 @@ public class ReconciliationApiTest extends ApiGatewayBaseTest {
     private static final String RECONCILIATION_ENDPOINT_PATH = "/reconciliation-report/{date}";
 
     @BeforeAll
-    static void setup() throws Exception {
+    static void setup() {
         loadConfig();
     }
 
-    //OLD APIM tests
-    @Disabled
-    @Test
-    void should_accept_request_with_valid_certificate_and_valid_subscription_key() throws Exception {
-        Response response = callReconciliationEndpoint(validClientKeyStore, validSubscriptionKey);
-
-        assertThat(response.getStatusCode()).isEqualTo(BAD_REQUEST.value());
-    }
-
-    @Disabled
-    @Test
-    void should_reject_request_with_invalid_subscription_key() throws Exception {
-        Response response = callReconciliationEndpoint(validClientKeyStore, "invalid-subscription-key");
-
-        assertThat(response.statusCode()).isEqualTo(UNAUTHORIZED.value());
-        assertThat(response.body().asString()).contains("Access denied due to invalid subscription key");
-    }
-
-    @Disabled
-    @Test
-    void should_reject_request_with_missing_subscription_header() throws Exception {
-        Response response = callReconciliationEndpoint(validClientKeyStore, null);
-
-        assertThat(response.statusCode()).isEqualTo(UNAUTHORIZED.value());
-        assertThat(response.body().asString()).contains("Access denied due to missing subscription key");
-    }
-
-    //NEW APIM TESTS
     @Test
     void should_reject_request_with_missing_jwt_token() {
         Response response = callReconciliationEndpointWithoutJwt();
@@ -75,34 +44,6 @@ public class ReconciliationApiTest extends ApiGatewayBaseTest {
 
         Response response = callReconciliationEndpointWithJwt(jwtAccessToken);
         assertThat(response.getStatusCode()).isEqualTo(BAD_REQUEST.value());
-    }
-
-
-    private Response callReconciliationEndpoint(
-        KeyStoreWithPassword clientKeyStore,
-        String subscriptionKey
-    ) throws Exception {
-        String statementsReport = "{\"test\": {}}";
-        RequestSpecification request = RestAssured
-            .given()
-            .baseUri(apiGatewayUrl)
-            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .header(SyntheticHeaders.SYNTHETIC_TEST_SOURCE, "Blob Router Service Smoke test")
-            .body(statementsReport);
-
-        if (clientKeyStore != null) {
-            request = request.config(
-                getSslConfigForClientCertificate(
-                    clientKeyStore.keyStore,
-                    clientKeyStore.password
-                )
-            );
-        }
-
-        if (subscriptionKey != null) {
-            request = request.header(SUBSCRIPTION_KEY_HEADER_NAME, subscriptionKey);
-        }
-        return request.post(RECONCILIATION_ENDPOINT_PATH, LocalDate.now().toString());
     }
 
     private Response callReconciliationEndpointWithJwt(String jwtAccessToken) {
