da06f367adbfe181be8c829e07bae76075344006
Josh <23316525+josh-uk@users.noreply.github.com>
2025-06-05 17:49:15 +0100

    Update everything (#2333)

--- src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/controllers/EnvelopeControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/blobrouter/controllers/EnvelopeControllerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import reactor.util.function.Tuples;
 import uk.gov.hmcts.reform.blobrouter.data.envelopes.Envelope;
@@ -46,7 +46,7 @@ public class EnvelopeControllerTest extends ControllerTestBase {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean private IncompleteEnvelopesService incompleteEnvelopesService;
+    @MockitoBean private IncompleteEnvelopesService incompleteEnvelopesService;
 
     @Test
     void should_find_envelope_by_file_name_and_container_if_it_exists() throws Exception {
